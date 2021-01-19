78d397ef69be15a3e55dd2fb249f36da23cdbd54
Tom Digby <tom@atomlearning.co.uk>
2021-01-19 12:20:31 +0000

    wip

--- dist/Box.d.ts
+++
@@ -1,5 +0,0 @@
-declare type Props = {
-    children?: any;
-};
-export declare const Box: (props: Props) => JSX.Element;
-export {};
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Select } from './Select';
-import { Box } from './Box';
-export { Select, Box };
