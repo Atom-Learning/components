7fada99374a29e25ae4c9d5fcec24cd8096b3eb2
Mariandi <mariandi.stylianou@atomlearning.co.uk>
2021-01-22 13:33:13 +0200

    feat: added a11y storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,6 +10,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-controls'
+    '@storybook/addon-controls',
+    '@storybook/addon-a11y'
   ]
 }
