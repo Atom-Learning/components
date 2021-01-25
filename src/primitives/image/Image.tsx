264c43f49eb00bd441787a6b833325eed26ea372
Gary Holland <gary@atomlearning.co.uk>
2021-01-25 16:42:21 +0000

    feat: simplify Image

--- src/primitives/image/Image.tsx
+++ src/primitives/image/Image.tsx
@@ -1,10 +1,4 @@
-import { StitchesProps } from '@stitches/react'
-import * as React from 'react'
-
 import { styled } from '../../stitches'
 
-export const BaseImage = styled('img', {})
+export const Image = styled('img', {})
 
-export const Image = (
-  props: StitchesProps<typeof BaseImage>
-): React.ReactElement => <BaseImage {...props} />
