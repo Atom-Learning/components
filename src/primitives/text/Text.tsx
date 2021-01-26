21a8b552fd3ab834de0ab20a068ecea7256815a4
Gary Holland <gary@atomlearning.co.uk>
2021-01-26 12:42:41 +0000

    feat: add Text docs

--- src/primitives/text/Text.tsx
+++ src/primitives/text/Text.tsx
@@ -1,3 +1,42 @@
-import { styled } from '../../stitches'
+import * as React from 'react'
 
-export const Text = styled('p', { fontFamily: 'sans', color: '$tonal900' })
+import { CSSBlob,styled } from '../../stitches'
+
+const BaseText = styled('p', {
+  fontFamily: 'sans',
+  color: '$tonal900',
+  fontWeight: 400,
+  maxWidth: '100ch',
+  variants: {
+    size: {
+      sm: {
+        fontSize: 'sm',
+        letterSpacing: '0.04em',
+        lineHeight: 1.6
+      },
+      md: {
+        fontSize: 'md',
+        letterSpacing: '0.02em',
+        lineHeight: 1.4
+      },
+      lg: {
+        fontSize: 'lg',
+        letterSpacing: '0.02em',
+        lineHeight: 1.4
+      }
+    }
+  }
+})
+
+type TextProps = {
+  css?: CSSBlob
+  size: 'sm' | 'md' | 'lg'
+}
+
+export const Text = (props: TextProps): React.ReactElement => {
+  return <BaseText {...props} />
+}
+
+Text.defaultProps = {
+  size: 'md'
+}
