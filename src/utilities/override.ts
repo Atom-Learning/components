8c672693e3da2afefa3b0bc5a53958069ac4a36e
Gary Holland <gary@atomlearning.co.uk>
2021-02-02 11:03:25 +0000

    feat: doccomment for Override type

--- src/utilities/override.ts
+++ src/utilities/override.ts
@@ -1,5 +1,24 @@
-// create a new type by overriding any of T's fields
-// with U's. It's like Object.assign for types.
-// Useful when StitchesProps<typeof {something}> is a little too permissive.
-
+/**
+ * Creates a new type by overriding any of T's fields
+ * with U's fields of the same name.
+ * It's like Object.assign() for types.
+ *
+ * @typeParam T - The base type
+ * @typeParam U - The type with the overrides you want to apply
+ *
+ * @example
+ * ```
+ * type BaseType = {
+ *  field1: "A" | "B" | "C"
+ *  field2: "D" | "E" | "F"
+ * }
+ *
+ * type NewType = Override<BaseType, { field2: "G" | "H" | "I" }
+ * //{
+ * // field1: "A" |"B" | "C"
+ * // field2: "G" | "H" | "I"
+ * //}
+ *
+ * ```
+ */
 export type Override<T, U> = Omit<T, keyof U> & U
