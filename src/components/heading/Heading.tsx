b68861b161c30d2a9607b96a2d87af96e9c33687
Gary Holland <gary@atomlearning.co.uk>
2021-02-08 11:28:40 +0000

    chore: type check Heading default props

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -7,11 +7,12 @@ const StyledHeading = styled('h1', {
   color: '$tonal-900',
   fontFamily: '$sans',
   fontWeight: '700',
+  lineHeight: '1.4',
   variants: {
     size: {
-      sm: { fontSize: '16px', letterSpacing: '0.01em', lineHeight: '1.4' },
-      md: { fontSize: '28px', letterSpacing: '0.02em', lineHeight: '1.4' },
-      lg: { fontSize: '37px', letterSpacing: '0.02em', lineHeight: '1.4' }
+      sm: { fontSize: '16px', letterSpacing: '0.01em' },
+      md: { fontSize: '28px', letterSpacing: '0.02em' },
+      lg: { fontSize: '37px', letterSpacing: '0.02em' }
     }
   }
 })
@@ -31,4 +32,4 @@ export const Heading: React.FC<HeadingProps> = (props) => {
 Heading.defaultProps = {
   as: 'h1',
   size: 'lg'
-}
+} as Partial<HeadingProps>
