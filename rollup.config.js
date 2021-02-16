4382e0c70bd9e509204d6ebf82ac4ca2919e7c10
Gary Holland <gary@atomlearning.co.uk>
2021-02-16 11:55:51 +0000

    feat: manage deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import bundleSize from 'rollup-plugin-bundle-size'
@@ -5,6 +6,8 @@ import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 const deps = Object.keys(pkg.dependencies || {})
 const peerDeps = Object.keys(pkg.peerDependencies || {})
@@ -17,10 +20,14 @@ export default {
     { file: pkg.module, format: 'esm', sourcemap: true }
   ],
   plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/env', '@babel/preset-react']
+    }),
     bundleSize(),
     commonjs(),
     resolve(),
-    terser(),
+    isProduction && terser(),
     typescript({
       rollupCommonJSResolveHack: false,
       clean: true
