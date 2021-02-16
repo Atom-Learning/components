a0e5654073ea87e9d74d433e89581d995104b849
Gary Holland <gary@atomlearning.co.uk>
2021-02-16 10:36:29 +0000

    feat: manually add dependencies to externals

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,12 @@ import typescript from 'rollup-plugin-typescript2'
 
 import pkg from './package.json'
 
+const deps = Object.keys(pkg.dependencies || {})
+const peerDeps = Object.keys(pkg.peerDependencies || {})
+
 export default {
   input: 'src/index.ts',
+  external: [...deps, ...peerDeps],
   output: [
     { file: pkg.main, format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'esm', sourcemap: true }
