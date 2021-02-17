0d8f8beb04b93d5b4e3e0437b0bb120d23eda1f7
Gary Holland <gary@atomlearning.co.uk>
2021-02-17 08:27:16 +0000

    chore: remove changes to sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,16 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, css, Text } from '../dist'
+import { Box, css } from '../dist'
 
 css.global(reset)()
 
 const App = () => {
-  return (
-    <Box>
-      <Text css={{ color: '$primary500' }}>hello</Text>
-    </Box>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
