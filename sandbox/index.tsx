8fd0aecf81bf805d9b54cd29aae821e82c1d5d78
Gary Holland <gary@atomlearning.co.uk>
2021-02-22 14:11:10 +0000

    chore: revert changes to sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,37 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, Flex, globalCss, InputField } from '../dist'
+import { Box, globalCss } from '../dist'
 
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Box css={{ p: '$3', width: '500px' }}>
-      <Flex>
-        <InputField
-          label="Name"
-          error="Name is a required field"
-          name="name"
-          placeholder="Your Name"
-          css={{ mb: '$3', mr: '$3' }}
-          required
-        />
-        <InputField
-          label="Email address"
-          name="email"
-          defaultValue="gary@atomlearning.co.uk"
-        />
-      </Flex>
-      <InputField
-        type="tel"
-        name="tel"
-        label="Phone number"
-        css={{ width: '30ch' }}
-        placeholder="0800 00 1066"
-      />
-    </Box>
-  )
+    return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
