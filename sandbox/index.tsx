9e243a69b6e100e66e4dcc0838aed9d9e9935999
Gary Holland <gary@atomlearning.co.uk>
2021-02-22 14:33:28 +0000

    feat: wip tooltip styling

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,12 +2,18 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss } from '../dist'
+import { Box, globalCss, Tooltip } from '../dist'
 
 globalCss(reset)()
 
 const App = () => {
-    return <Box />
+  return (
+    <Box css={{ paddingLeft: '$3' }}>
+      <Tooltip content="hi there">
+        <button>Hello</button>
+      </Tooltip>
+    </Box>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -1,14 +1,30 @@
+import { Slot } from '@radix-ui/react-slot'
 import * as RadixTooltip from '@radix-ui/react-tooltip'
 import * as React from 'react'
+
+import { styled } from '~/stitches'
+
+const StyledContent = styled(RadixTooltip.Content, {
+  borderRadius: '3', // '$0' is too high for the arrow to sit flush against the box if the arrow renders near a corner
+  padding: '$1 $2',
+  fontFamily: '$sans',
+  fontSize: 14,
+  backgroundColor: '$tonal300',
+  color: '$tonal900'
+})
+
+const StyledArrow = styled(RadixTooltip.Arrow, {
+  fill: '$tonal300'
+})
 
 export const Tooltip = ({ children, content }) => {
   return (
     <RadixTooltip.Root>
-      <RadixTooltip.Trigger>{children}</RadixTooltip.Trigger>
-      <RadixTooltip.Content>
+      <RadixTooltip.Trigger as={Slot}>{children}</RadixTooltip.Trigger>
+      <StyledContent>
         {content}
-        <RadixTooltip.Arrow />
-      </RadixTooltip.Content>
+        <StyledArrow />
+      </StyledContent>
     </RadixTooltip.Root>
   )
 }
