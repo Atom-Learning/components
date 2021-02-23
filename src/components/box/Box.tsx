f5d4ec3a2462116ecd51c8b5deec6cf0b36b1d74
Gary Holland <gary@atomlearning.co.uk>
2021-02-23 15:42:41 +0000

    feat: better temporary ref fix

--- src/components/box/Box.tsx
+++ src/components/box/Box.tsx
@@ -3,15 +3,11 @@ import * as React from 'react'
 import { styled } from '~/stitches'
 
 export const StyledBox = styled('div', {})
-type BoxProps = React.ComponentProps<typeof StyledBox>
 
 // wrapping the stitches component in a React.FC prevents a TS error
 // when passing custom components as children
-export const Box: React.FC<BoxProps> = React.forwardRef<
-  HTMLElement,
-  BoxProps
->((props, ref) => {
-  return <StyledBox ref={ref} {...props} />
-})
+export const Box: React.FC<React.ComponentPropsWithoutRef<typeof StyledBox>> = (
+  props
+) => <StyledBox {...props} />
 
 Box.displayName = 'Box'
--- src/components/tooltip/Tooltip.tsx
+++ src/components/tooltip/Tooltip.tsx
@@ -2,7 +2,6 @@ import { Slot } from '@radix-ui/react-slot'
 import * as RadixTooltip from '@radix-ui/react-tooltip'
 import * as React from 'react'
 
-import { Box } from '~/components/box'
 import { styled } from '~/stitches'
 
 const StyledContent = styled(RadixTooltip.Content, {
@@ -22,17 +21,20 @@ type TooltipProps = {
   content: React.ReactNode
 }
 
+// We need a component that can accept a ref for the popover to target
+// TODO: replace with a Box once we make that accept a ref
+const TriggerWrapper = React.forwardRef<
+  HTMLSpanElement,
+  { children: React.ReactNode }
+>((props, ref) => <span ref={ref} {...props} />)
+
 export const Tooltip: React.FC<TooltipProps> = ({ children, content }) => {
-  const triggerContainerRef = React.useRef<HTMLElement>(null)
-  // const triggerContainerRef = React.useRef<HTMLDivElement>(null)
+  const triggerContainerRef = React.useRef<HTMLDivElement>(null)
 
   return (
     <RadixTooltip.Root>
       <RadixTooltip.Trigger as={Slot}>
-        {/* inline Box so that the Tooltip knows the real size of the trigger */}
-        <Box ref={triggerContainerRef} css={{ display: 'inline' }}>
-          {children}
-        </Box>
+        <TriggerWrapper ref={triggerContainerRef}>{children}</TriggerWrapper>
       </RadixTooltip.Trigger>
       <StyledContent side="top" anchorRef={triggerContainerRef}>
         {content}
