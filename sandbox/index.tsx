be677b66bac5db0145cb344962af879aee790f24
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2021-02-24 16:44:36 +0200

    refactor: reverting changes in sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -3,23 +3,11 @@ import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
 import { Box, globalCss } from '../dist'
-import { Video } from '../src/components/video/Video'
 
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Box
-      css={{
-        justifyContent: 'space-between',
-        gap: 10,
-        flexDirection: 'column',
-        flexWrap: 'wrap'
-      }}
-    >
-      <Video externalId="1084537" width="30%" height="30%" />
-    </Box>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
