50dc62227ba64e2220376c7f1a4739da91393fc9
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2021-02-24 16:03:39 +0200

    feat: commiting sandbox for testing

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -3,11 +3,23 @@ import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
 import { Box, globalCss } from '../dist'
+import { Video } from '../src/components/video/Video'
 
 globalCss(reset)()
 
 const App = () => {
-    return <Box />
+  return (
+    <Box
+      css={{
+        justifyContent: 'space-between',
+        gap: 10,
+        flexDirection: 'column',
+        flexWrap: 'wrap'
+      }}
+    >
+      <Video externalId="1084537" width="30%" height="30%" />
+    </Box>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
