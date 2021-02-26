26c06b6a05afa0f670488124154a89e6e38a42f8
Gary Holland <gary@atomlearning.co.uk>
2021-02-26 12:18:57 +0000

    chore: revert changes to sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,25 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Flex, globalCss, Loader, Button } from '../dist'
+import { Box, globalCss } from '../dist'
 
 globalCss(reset)()
 
 const App = () => {
-  const [isLoading, setIsLoading] = React.useState(true)
-  return (
-    <Flex
-      css={{
-        color: 'black',
-        flexDirection: 'column',
-        width: '200px',
-        alignItems: 'center'
-      }}
-    >
-      <Button onClick={() => setIsLoading((curr) => !curr)}>Toggle</Button>
-      {isLoading && <Loader />}
-    </Flex>
-  )
+    return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
