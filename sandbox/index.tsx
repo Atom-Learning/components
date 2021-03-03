89c7d6a38e5294fa8f7f4406488daa2366b24687
Gary Holland <gary@atomlearning.co.uk>
2021-03-03 16:31:04 +0000

    chore: expand on example

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -8,23 +8,34 @@ import { Button, Form, globalCss, InputField } from '../dist'
 globalCss(reset)()
 
 const App = () => {
-  const data = { name: 'Gary' }
+  const defaultValues = { name: 'Gary', age: 26 }
 
   const handleSubmit = (data) => {
     console.log('data:', data)
+    // data: Object { name: "Gary", age: "26" }
   }
 
   return (
     <Form
       onSubmit={handleSubmit}
-      defaultValues={data}
+      defaultValues={defaultValues}
       css={{ width: '300px', padding: '$2 $3' }}
     >
       <InputField
         name="name"
         label="Name"
         validation={{
-          maxLength: 1
+          required: 'You must have a name'
+        }}
+      />
+
+      <InputField
+        name="age"
+        label="Age"
+        type="number"
+        validation={{
+          required: 'You must have an age',
+          min: { value: 0, message: "Negative ages don't make sense!" }
         }}
       />
       <Button>Submit</Button>
