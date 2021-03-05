f45f54eb4b997ceb30b36bc35fb2b7fca3d2f5a8
Gary Holland <gary@atomlearning.co.uk>
2021-03-05 10:03:46 +0000

    chore: remove changes to sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,17 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss, InputField,PasswordField } from '../dist'
+import { Box, globalCss } from '../dist'
 
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Box css={{ width: '300px', margin: '0 auto' }}>
-      <InputField name="username" label="Username" css={{ mb: '$3' }} />
-      <PasswordField />
-    </Box>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
