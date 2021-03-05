48023d5015ef17aa292b578594db84b7c154c431
Gary Holland <gary@atomlearning.co.uk>
2021-03-05 09:45:35 +0000

    feat: ad PasswordField tests

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,12 +2,17 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss } from '../dist'
+import { Box, globalCss, InputField,PasswordField } from '../dist'
 
 globalCss(reset)()
 
 const App = () => {
-  return <Box />
+  return (
+    <Box css={{ width: '300px', margin: '0 auto' }}>
+      <InputField name="username" label="Username" css={{ mb: '$3' }} />
+      <PasswordField />
+    </Box>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/components/password-field/PasswordField.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import { useState } from 'react'
+
+import { Box } from '~/components/box'
+import { Flex } from '~/components/flex'
+import { Eye, EyeOff, Icon } from '~/components/icon'
+import { Input, InputProps } from '~/components/input'
+import { Label } from '~/components/label'
+import { Link } from '~/components/link'
+import { styled } from '~/stitches'
+
+type PasswordFieldProps = InputProps & { label: string }
+
+export const PasswordField: React.FC<PasswordFieldProps> = ({
+  css = {},
+  name = 'password',
+  ...remainingProps
+}) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false)
+  const togglePasswordVisibility = () =>
+    setIsPasswordVisible((currentState) => !currentState)
+
+  return (
+    <Box css={{ position: 'relative', ...(css as any) }}>
+      <Flex
+        css={{
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          mb: '$1',
+          fontFamily: 'sans',
+          fontSize: 'sm'
+        }}
+      >
+        <Label htmlFor={name}> Password</Label>
+        <Link
+          href="https://app.atomlearning.co.uk/forgotten"
+          css={{ fontSize: '$sm' }}
+        >
+          Forgot your password?
+        </Link>
+      </Flex>
+
+      <Input
+        type={isPasswordVisible ? 'text' : 'password'}
+        css={{ mb: '$1', paddingRight: 24 }}
+        autoComplete="current-password"
+        name={name}
+        id={name}
+        {...remainingProps}
+      />
+      <InvisibleButton
+        onClick={togglePasswordVisibility}
+        aria-label={isPasswordVisible ? 'Hide password' : 'Show password'}
+      >
+        <Icon is={isPasswordVisible ? Eye : EyeOff} />
+      </InvisibleButton>
+    </Box>
+  )
+}
+
+PasswordField.displayName = 'PasswordField'
+
+const InvisibleButton = styled('button', {
+  border: 'none',
+  padding: 0,
+  background: 'none',
+  cursor: 'pointer',
+  position: 'absolute',
+  bottom: 16,
+  right: 8
+})
