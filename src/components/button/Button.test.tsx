4160d0a33704a90c6d31d1f245e3b39e880d4bfa
Tom Digby <tom@atomlearning.co.uk>
2021-03-10 10:27:52 +0000

    fix: update tests and fix snapshots

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -73,7 +73,7 @@ describe(`Button component`, () => {
         </Button>
       )
 
-      expect(await screen.queryByText('BUTTON')).not.toBeInTheDocument()
+      expect(await screen.queryByText('BUTTON')).not.toBeVisible()
       expect(await screen.getByRole('alert')).toBeInTheDocument()
 
       expect(container).toMatchSnapshot()
