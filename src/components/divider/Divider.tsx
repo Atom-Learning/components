48b763818da5086f463fdf4a03596ea1656c75f8
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2021-03-24 11:47:11 +0200

    feat: add divider component

---
+++ src/components/divider/Divider.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+import { styled } from '~/stitches'
+
+const StyledDivider = styled('hr', {
+  border: 0,
+  borderTop: '1px solid $tonal400',
+  margin: '2rem auto',
+  maxWidth: '50%'
+})
+
+type DividerProps = typeof StyledDivider
+
+export const Divider: React.FC<DividerProps> = (props) => {
+  return <StyledDivider {...props} />
+}
+
+Divider.displayName = 'Divider'
