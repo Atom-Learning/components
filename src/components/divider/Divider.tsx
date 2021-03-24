90f73e0459b57c240026f943eada2e82e3664985
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2021-03-24 14:21:57 +0200

    fix: removed unnecessary export

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -2,18 +2,10 @@ import * as React from 'react'
 
 import { styled } from '~/stitches'
 
-const StyledDivider = styled('hr', {
+export const Divider = styled('hr', {
   border: 0,
   borderTop: '1px solid $tonal400',
   my: '$4',
   mx: 'auto',
   maxWidth: '50%'
 })
-
-type DividerProps = typeof StyledDivider
-
-export const Divider: React.FC<DividerProps> = (props) => {
-  return <StyledDivider {...props} />
-}
-
-Divider.displayName = 'Divider'
