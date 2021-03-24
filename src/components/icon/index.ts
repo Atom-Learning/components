5cb154c3ba553ed422daba7dbe67d575c53cac2e
Gary Holland <gary@atomlearning.co.uk>
2021-03-24 11:36:55 +0000

    chore: remove unused icons

--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,2 +1,2 @@
 export { Icon } from './Icon'
-export { Check, ChevronLeft, ChevronRight, Home, Eye, EyeOff, X } from './svg'
+export { Check, Home, Eye, EyeOff, X } from './svg'
--- src/components/icon/svg/index.ts
+++ src/components/icon/svg/index.ts
@@ -1,6 +1,4 @@
 export { ReactComponent as Check } from './check.svg'
-export { ReactComponent as ChevronLeft } from './chevron-left.svg'
-export { ReactComponent as ChevronRight } from './chevron-right.svg'
 export { ReactComponent as Eye } from './eye.svg'
 export { ReactComponent as EyeOff } from './eye-off.svg'
 export { ReactComponent as Home } from './home.svg'
