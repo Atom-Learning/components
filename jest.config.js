69138577cbf5c16a94e34495306107b8ab4de2d9
Tom Digby <tom@atomlearning.co.uk>
2021-03-25 16:26:07 +0000

    fix: upgrade icons package, fix react path in tests

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/'],
   setupFilesAfterEnv: ['./jest-setup.ts'],
   moduleNameMapper: {
-    '~/(.*)': '<rootDir>/src/$1' // matches the path alias in tsconfig.json
+    '~/(.*)': '<rootDir>/src/$1', // matches the path alias in tsconfig.json
+    '^react$': '<rootDir>/node_modules/react'
   },
   transform: {
     '^.+\\.(ts|tsx)$': 'babel-jest'
