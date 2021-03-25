1f972140289c1d0ad69dd4c74cb6dfca97b1beb8
Tom Digby <tom@atomlearning.co.uk>
2021-03-25 17:33:03 +0000

    fix: revert sandbox changes

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -1,57 +1,14 @@
 import 'regenerator-runtime/runtime'
 
-import { Danger } from '@atom-learning/icons'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import {
-  Button,
-  Checkbox,
-  Flex,
-  globalCss,
-  Icon,
-  Input,
-  Loader,
-  PasswordField,
-  ProgressBar,
-  RadioButton,
-  RadioButtonGroup,
-  Select,
-  Textarea
-} from '../dist'
+import { Box, globalCss } from '../dist'
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Flex
-      css={{
-        flexDirection: 'column',
-        gap: '$4',
-        height: '100vh',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Button theme="warning">
-        <Icon is={Danger} css={{ mr: '$2' }} />
-        Warning button
-      </Button>
-      <Checkbox />
-      <Input placeholder="Test" css={{ width: 340 }} />
-      <ProgressBar value={30} css={{ width: 340 }} />
-      <RadioButtonGroup>
-        <RadioButton />
-      </RadioButtonGroup>
-      <Select
-        options={[{ label: 'What', value: 'Hello' }]}
-        css={{ width: 340 }}
-      />
-      <Textarea css={{ width: 340 }} />
-      <Loader size="lg" />
-      <PasswordField css={{ width: 340 }} />
-    </Flex>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
