248e089a2b6d464cecd751bbe371f6e7a8b29689
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2021-03-29 17:51:02 +0300

    fix: updated accessibility tests for radio button

--- src/components/radio/RadioButton.test.tsx
+++ src/components/radio/RadioButton.test.tsx
@@ -15,24 +15,24 @@ describe(`Radio component`, () => {
       </IdProvider>
     )
 
-    // filtering on hidden is needed to find the visibile item as Radix adds a hidden input before the visible button
-    await screen.getByRole('radio', { hidden: false })
+    await screen.getByRole('radio')
 
     expect(container).toMatchSnapshot()
   })
 
   it('renders a radio button - has no programmatically detectable a11y issues', async () => {
-    render(
-      <IdProvider>
-        <RadioButtonGroup>
-          <RadioButton value="value" aria-label="indicator" />
-        </RadioButtonGroup>
-      </IdProvider>
+    const { container } = render(
+      <label>
+        Label
+        <IdProvider>
+          <RadioButtonGroup>
+            <RadioButton value="value" aria-label="indicator" />
+          </RadioButtonGroup>
+        </IdProvider>
+      </label>
     )
 
-    // filtering on hidden is needed to find the visibile item as Radix adds a hidden input before the visible button
-    const radio = await screen.getByRole('radio', { hidden: false })
-    expect(await axe(radio)).toHaveNoViolations()
+    expect(await axe(container)).toHaveNoViolations()
   })
 
   it('renders a disabled radio button', async () => {
@@ -43,23 +43,30 @@ describe(`Radio component`, () => {
         </RadioButtonGroup>
       </IdProvider>
     )
-    // filtering on hidden is needed to find the visibile item as Radix adds a hidden input before the visible button
-    await screen.getByRole('radio', { hidden: false })
+
+    await screen.getByRole('radio')
 
     expect(container).toMatchSnapshot()
   })
 
   it('renders a disabled radio button - has no programmatically detectable a11y issues', async () => {
-    render(
-      <IdProvider>
-        <RadioButtonGroup>
-          <RadioButton value="value" aria-label="disabled indicator" disabled />
-        </RadioButtonGroup>
-      </IdProvider>
+    const { container } = render(
+      <label>
+        Label
+        <IdProvider>
+          <RadioButtonGroup>
+            <RadioButton
+              value="value"
+              aria-label="disabled indicator"
+              disabled
+            />
+          </RadioButtonGroup>
+        </IdProvider>
+      </label>
     )
-    // filtering on hidden is needed to find the visibile item as Radix adds a hidden input before the visible button
-    const radio = await screen.getByRole('radio', { hidden: false })
 
-    expect(await axe(radio)).toHaveNoViolations()
+    await screen.getByRole('radio')
+
+    expect(await axe(container)).toHaveNoViolations()
   })
 })
