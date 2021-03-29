1b66254a67f5d449e87d60ff8b9e3ceb0eb2f445
Gary Holland <gary@atomlearning.co.uk>
2021-03-29 15:01:05 +0100

    chore: remove commented code

--- src/components/carousel/Slide.tsx
+++ src/components/carousel/Slide.tsx
@@ -4,18 +4,7 @@ import * as React from 'react'
 import { Box } from '~/components'
 import { styled } from '~/stitches'
 
-const StyledSlide = styled(BaseSlide, {
-  // opacity: '0.7',
-  // transition: 'all 0.25s ease-in',
-  // '&[aria-selected="true"]': {
-  //   opacity: 1,
-  //   // apply box shadow to the Slide content directly,
-  //   // not the wrappers added internally
-  //   '& > div > div > *': {
-  //     boxShadow: '$1'
-  //   }
-  // }
-})
+const StyledSlide = styled(BaseSlide, {})
 
 type SlideProps = React.ComponentProps<typeof StyledSlide> & { index: number }
 
