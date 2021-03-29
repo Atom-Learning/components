7274380285a25ef374968b7fd9d19e1ab8abb830
Tom Digby <tom@atomlearning.co.uk>
2021-03-29 09:41:28 +0100

    feat: simplify implementation

--- src/components/stack/Stack.tsx
+++ src/components/stack/Stack.tsx
@@ -1,60 +1,59 @@
-import { styled, theme } from '~/stitches'
+import theme from '@atom-learning/theme'
+import * as React from 'react'
 
-const child = '> *'
-const childWithGap = '> * + *'
+import { CSS, styled } from '~/stitches'
 
-const gap = Object.values(theme.space).reduce(
-  (acc, { token }) => ({
+const gap = Object.keys(theme.space).reduce(
+  (acc, key) => ({
     ...acc,
-    [token]: {
-      [childWithGap]: { $$stackGap: `$space$${token}` }
+    [key]: {
+      mt: `-$space$${key}`,
+      ml: `-$space$${key}`,
+      '& > *': {
+        mt: `$space$${key}`,
+        ml: `$space$${key}`
+      }
     }
   }),
   {}
-)
+) as Record<keyof typeof theme.space, CSS>
 
-export const Stack = styled('div', {
+const StyledStack = styled('div', {
   display: 'flex',
-  [child]: {
-    margin: 0
-  },
-  [childWithGap]: {
-    $$stackGapLeft: 'initial',
-    $$stackGapRight: 'initial',
-    $$stackGapTop: 'initial',
-    marginLeft: '$$stackGapLeft',
-    marginTop: '$$stackGapTop',
-    marginRight: '$$stackGapRight'
+  flexWrap: 'wrap',
+  width: 'max-content',
+  '& > *': {
+    m: 0
   },
   variants: {
     direction: {
       row: {
         flexDirection: 'row',
-        alignItems: 'center',
-        [childWithGap]: {
-          $$stackGapLeft: '$$stackGap'
-        }
+        alignItems: 'center'
       },
       'row-reverse': {
         flexDirection: 'row-reverse',
-        alignItems: 'center',
-        [childWithGap]: {
-          $$stackGapRight: '$$stackGap'
-        }
+        alignItems: 'center'
       },
       column: {
-        flexDirection: 'column',
-        [childWithGap]: {
-          $$stackGapTop: '$$stackGap'
-        }
+        flexDirection: 'column'
       }
     },
     gap
-  },
-  defaultVariants: {
-    gap: 2,
-    direction: 'row'
   }
 })
+
+type StackProps = {
+  as?: 'div'
+  // TODO use keys from theme.space
+  gap?: 0 | 1 | 2 | 3 | 4 | 5 | 6
+  direction?: 'column' | 'row' | 'row-reverse'
+}
+
+export const Stack: React.FC<StackProps> = ({
+  gap = 2,
+  direction = 'row',
+  ...remainingProps
+}) => <StyledStack gap={gap} direction={direction} {...remainingProps} />
 
 Stack.displayName = 'Stack'
