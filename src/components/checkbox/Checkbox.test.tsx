fe65633ca89b7591ef32deb4d23424d2145b4431
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2021-03-29 14:40:36 +0300

    test: separated tests for checkbox, form & heading

--- src/components/checkbox/Checkbox.test.tsx
+++ src/components/checkbox/Checkbox.test.tsx
@@ -20,7 +20,7 @@ describe(`Checkbox component`, () => {
     expect(container).toMatchSnapshot()
   })
 
-  it(' has no programmatically detectable a11y issues', async () => {
+  it('has no programmatically detectable a11y issues', async () => {
     const { container } = render(
       // workaround for failing axe test -
       // Radix's checkbox renders an <input type="checkbox" hidden=""/> but
--- src/components/heading/Heading.test.tsx
+++ src/components/heading/Heading.test.tsx
@@ -11,6 +11,11 @@ describe(`Heading component`, () => {
     await screen.getByText('HEADING')
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('has no programmatically detectable a11y issues', async () => {
+    const { container } = render(<Heading>HEADING</Heading>)
+
     expect(await axe(container)).toHaveNoViolations()
   })
 })
