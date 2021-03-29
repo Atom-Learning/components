63831a266c08742dac2c5665d6410531293a1c12
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2021-03-29 14:42:07 +0300

    fix: fix test using missing svg separated concerns

--- src/components/icon/Icon.test.tsx
+++ src/components/icon/Icon.test.tsx
@@ -1,16 +1,22 @@
+import { Ok } from '@atom-learning/icons'
 import { render, screen } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import * as React from 'react'
 
-import { Check, Icon } from '.'
+import { Icon } from '.'
 
 describe(`Icon component`, () => {
   it('renders an icon', async () => {
-    const { container } = render(<Icon is={Check} title="check" />)
+    const { container } = render(<Icon is={Ok} title="check" />)
 
     await screen.getByTitle('check')
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('has no programmatically detectable a11y issues', async () => {
+    const { container } = render(<Icon is={Ok} title="check" />)
+
     expect(await axe(container)).toHaveNoViolations()
   })
 })
