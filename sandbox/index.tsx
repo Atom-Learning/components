be9691b29562ebca188daf8b23268348ce1cccdc
Gary Holland <gary@atomlearning.co.uk>
2021-03-29 10:56:34 +0100

    feat: implement fukol grid

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -4,11 +4,19 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss } from '../dist'
+import { Box, globalCss, Grid, Image } from '../dist'
 globalCss(reset)()
 
 const App = () => {
-  return <Box />
+  return (
+    <Box>
+      <Grid basis="0.5em" gap="0.05em">
+        {[0, 1, 2, 3].map((num) => (
+          <Image key={num} src="https://placekitten.com/200/300" alt="" />
+        ))}
+      </Grid>
+    </Box>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,5 +1,41 @@
-import { styled } from '~/stitches'
+import * as React from 'react'
 
-export const Grid = styled('div', { display: 'grid' })
+import { Flex } from '~/components/flex'
+import { CSS } from '~/stitches'
+
+// Grid implementation taken from  https://github.com/Heydon/fukol-grids
+
+type GridProps = {
+  basis: string
+  css: CSS
+  gap: string
+} & React.ComponentProps<typeof Flex>
+
+export const Grid: React.FC<GridProps> = ({
+  basis,
+  children,
+  gap,
+  css,
+  ...remainingProps
+}) => {
+  return (
+    <Flex
+      css={
+        {
+          flexWrap: 'wrap',
+          margin: `-${gap}`,
+          '& > *': {
+            flex: `1 0 ${basis}`,
+            margin: gap
+          },
+          ...css
+        } as CSS
+      }
+      {...remainingProps}
+    >
+      {children}
+    </Flex>
+  )
+}
 
 Grid.displayName = 'Grid'
