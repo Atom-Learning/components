49ff81895a9827d616ce9152f934c660166882e9
Tom Digby <tom@atomlearning.co.uk>
2021-03-29 14:32:57 +0100

    fix: snapshot, remove a11y test

--- src/components/stack/Stack.test.tsx
+++ src/components/stack/Stack.test.tsx
@@ -1,14 +1,12 @@
 import { render } from '@testing-library/react'
-import { axe } from 'jest-axe'
 import * as React from 'react'
 
 import { Stack } from '.'
 
-describe("Stack component", () => {
+describe('Stack component', () => {
   it('renders', async () => {
     const { container } = render(<Stack />)
 
     expect(container).toMatchSnapshot()
-    expect(await axe(container)).toHaveNoViolations()
   })
 })
