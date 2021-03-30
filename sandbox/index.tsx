8fdd3a72a3248f5b61fac3b936cc5ca8c2284a91
Gary Holland <gary@atomlearning.co.uk>
2021-03-30 10:15:27 +0100

    chore: remove changes to sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -4,43 +4,11 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, Carousel, globalCss, Image, Flex } from '../dist'
+import { Box, globalCss } from '../dist'
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Box css={{ width: '500px', p: '$3', m: '0 auto' }}>
-      <Carousel slideWidth={200} slideHeight={300} numSlides={4}>
-        <Flex css={{ flexDirection: 'column' }}>
-          <Box css={{ position: 'relative' }}>
-            <Carousel.Arrows />
-            <Carousel.Slider aria-label={'Example carousel'} trayTag="div">
-              {[0, 1, 2, 3].map((num) => (
-                <Carousel.Slide
-                  key={num}
-                  index={num}
-                  css={{
-                    opacity: '0.7',
-                    transition: 'all 0.25s ease-in',
-                    '&[aria-selected="true"]': {
-                      opacity: 1,
-                      '& > div > div > *': {
-                        boxShadow: '$1'
-                      }
-                    }
-                  }}
-                >
-                  <Image src="https://placekitten.com/200/300" alt="" />
-                </Carousel.Slide>
-              ))}
-            </Carousel.Slider>
-          </Box>
-
-          <Carousel.Pagination />
-        </Flex>
-      </Carousel>
-    </Box>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
