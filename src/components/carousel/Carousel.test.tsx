6042784f4e1a78059546869156f17b1f6d56b24b
Gary Holland <gary@atomlearning.co.uk>
2021-03-30 10:14:54 +0100

    chore: separate tests

--- src/components/carousel/Carousel.test.tsx
+++ src/components/carousel/Carousel.test.tsx
@@ -36,6 +36,37 @@ describe('Carousel component', () => {
     )
 
     expect(container).toMatchSnapshot()
+  })
+
+  it('has no programatically detectable accesibility issues', async () => {
+    const { container } = render(
+      <Carousel
+        slideWidth={200}
+        slideHeight={300}
+        type="overflow"
+        numSlides={4}
+      >
+        <Flex css={{ flexDirection: 'column' }}>
+          <Box css={{ position: 'relative' }}>
+            <Carousel.Arrows />
+            <Carousel.Slider aria-label="Example carousel" trayTag="div">
+              {[0, 1, 2, 3].map((num) => (
+                <Carousel.Slide
+                  key={num}
+                  index={num}
+                  aria-label={`slide ${num}`}
+                >
+                  Slide {num}
+                </Carousel.Slide>
+              ))}
+            </Carousel.Slider>
+          </Box>
+
+          <Carousel.Pagination />
+        </Flex>
+      </Carousel>
+    )
+
     expect(await axe(container)).toHaveNoViolations()
   })
 })
