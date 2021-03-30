b9fba87dce670133d4e4750aae0ff25e5c915134
Gary Holland <gary@atomlearning.co.uk>
2021-03-30 09:31:21 +0100

    chore: remove changes to sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -4,19 +4,11 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss, Grid, Image } from '../dist'
+import { Box, globalCss } from '../dist'
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Box>
-      <Grid basis="0.5em" gap={1}>
-        {[0, 1, 2, 3].map((num) => (
-          <Image key={num} src="https://placekitten.com/200/300" alt="" />
-        ))}
-      </Grid>
-    </Box>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
