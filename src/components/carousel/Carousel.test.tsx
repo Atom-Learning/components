c7fc6b55a88d2d51bc958585b9089f828d88bf63
Gary Holland <gary@atomlearning.co.uk>
2021-03-30 11:27:13 +0100

    chore: simplify example carousels

--- src/components/carousel/Carousel.test.tsx
+++ src/components/carousel/Carousel.test.tsx
@@ -2,60 +2,53 @@ import { render } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import * as React from 'react'
 
-import { Box, Flex, Image } from '../'
+import { Image } from '../'
 import { Carousel } from '.'
+
+const ExampleCarousel = () => (
+  <Carousel
+    slideWidth={200}
+    slideHeight={300}
+    numSlides={4}
+    css={{ display: 'flex', flexDirection: 'column', position: 'relative' }}
+  >
+    <Carousel.Arrows />
+    <Carousel.Slider aria-label="Example carousel" trayTag="div">
+      {[0, 1, 2, 3].map((num) => (
+        <Carousel.Slide
+          key={num}
+          index={num}
+          aria-label={`slide ${num}`}
+          css={{
+            opacity: '0.7',
+            transition: 'all 0.25s ease-in',
+            '&[aria-selected="true"]': {
+              opacity: 1,
+              // apply box shadow to the Slide content directly,
+              // not the wrappers added internally
+              '& > div > div > *': {
+                boxShadow: '$1'
+              }
+            }
+          }}
+        >
+          <Image alt="" src="https://placekitten.com/200/300" />
+        </Carousel.Slide>
+      ))}
+    </Carousel.Slider>
+    <Carousel.Pagination />
+  </Carousel>
+)
 
 describe('Carousel component', () => {
   it('renders', async () => {
-    const { container } = render(
-      <Carousel slideWidth={200} slideHeight={300} numSlides={4}>
-        <Flex css={{ flexDirection: 'column' }}>
-          <Box css={{ position: 'relative' }}>
-            <Carousel.Arrows />
-            <Carousel.Slider aria-label="Example carousel" trayTag="div">
-              {[0, 1, 2, 3].map((num) => (
-                <Carousel.Slide
-                  key={num}
-                  index={num}
-                  aria-label={`slide ${num}`}
-                >
-                  Slide {num}
-                </Carousel.Slide>
-              ))}
-            </Carousel.Slider>
-          </Box>
-
-          <Carousel.Pagination />
-        </Flex>
-      </Carousel>
-    )
+    const { container } = render(<ExampleCarousel />)
 
     expect(container).toMatchSnapshot()
   })
 
   it('has no programatically detectable accesibility issues', async () => {
-    const { container } = render(
-      <Carousel slideWidth={200} slideHeight={300} numSlides={4}>
-        <Flex css={{ flexDirection: 'column' }}>
-          <Box css={{ position: 'relative' }}>
-            <Carousel.Arrows />
-            <Carousel.Slider aria-label="Example carousel" trayTag="div">
-              {[0, 1, 2, 3].map((num) => (
-                <Carousel.Slide
-                  key={num}
-                  index={num}
-                  aria-label={`slide ${num}`}
-                >
-                  Slide {num}
-                </Carousel.Slide>
-              ))}
-            </Carousel.Slider>
-          </Box>
-
-          <Carousel.Pagination />
-        </Flex>
-      </Carousel>
-    )
+    const { container } = render(<ExampleCarousel />)
 
     expect(await axe(container)).toHaveNoViolations()
   })
