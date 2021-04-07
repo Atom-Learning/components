f77c98846af4a322bc1cb8177df364860bb02ba9
Gary Holland <gary@atomlearning.co.uk>
2021-04-07 09:10:19 +0100

    chore: remove sandbox changes

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -4,26 +4,16 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import {
-  Button,
-  CheckboxField,
-  Form,
-  globalCss,
-  RadioGroupField
-} from '../dist'
-import { Checkbox } from '@radix-ui/react-checkbox'
+import { Box, globalCss, PasswordField } from '../dist'
 globalCss(reset)()
 
 const App = () => {
   return (
-    <Form onSubmit={console.log} css={{ mx: 'auto', width: '500px', p: '$2' }}>
-      <RadioGroupField name="option" label="Options" defaultValue={'1'}>
-        <RadioGroupField.Item value="1" label="1" />
-        <RadioGroupField.Item value="2" label="2" />
-      </RadioGroupField>
-      <CheckboxField name="yes?" label="Yes?" />
-      <Button type="submit">Submit</Button>
-    </Form>
+    <PasswordField
+      label="Password"
+      name="password"
+      prompt={{ label: 'Forgot your password?', link: 'https://google.co.uk' }}
+    />
   )
 }
 
