0da11ae5067b97b066af4f66a3a0e40b26ffb453
Gary Holland <gary@atomlearning.co.uk>
2021-04-21 11:09:37 +0100

    feat: update Form to take render prop

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -4,12 +4,36 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss } from '../dist'
+import { Box, Button, Form, globalCss, Input, InputField } from '../dist'
 
 globalCss(reset)()
 
+const FormContent = ({ formState }) => (
+  <>
+    <InputField
+      name="name"
+      label="Name"
+      validation={{
+        required: 'Name is required!',
+        minLength: { value: 3, message: 'longer!' }
+      }}
+    />
+    <Button type="submit" disabled={!formState.isValid}>
+      Submit
+    </Button>
+  </>
+)
+
 const App = () => {
-  return <Box />
+  return (
+    <Box>
+      <Form
+        onSubmit={console.log}
+        // defaultValues={{ name: 'gary' }}
+        render={FormContent}
+      />
+    </Box>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
