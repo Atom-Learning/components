f91a4b7687ff3d8344b4efea033ec17ddbd75dc6
Gary Holland <gary@atomlearning.co.uk>
2021-04-28 16:11:25 +0100

    chore: update test a11y workaround

--- src/components/combobox/Combobox.test.tsx
+++ src/components/combobox/Combobox.test.tsx
@@ -2,24 +2,20 @@ import { fireEvent, render, screen } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import * as React from 'react'
 
-import { Label } from '../label'
 import { Combobox } from '.'
 
 const SimpleExample = () => (
-  <>
-    <Label htmlFor="fruit">What's your favourite fruit?</Label>
-    <Combobox openOnFocus>
-      <Combobox.Input id="fruit" />
-      <Combobox.Popover>
-        <Combobox.List>
-          <Combobox.Option value="Apple" />
-          <Combobox.Option value="Banana" />
-          <Combobox.Option value="Cranberry" />
-          <Combobox.Option value="Dragon fruit" />
-        </Combobox.List>
-      </Combobox.Popover>
-    </Combobox>
-  </>
+  <Combobox openOnFocus aria-label="Favourite fruit">
+    <Combobox.Input id="fruit" />
+    <Combobox.Popover>
+      <Combobox.List>
+        <Combobox.Option value="Apple" />
+        <Combobox.Option value="Banana" />
+        <Combobox.Option value="Cranberry" />
+        <Combobox.Option value="Dragon fruit" />
+      </Combobox.List>
+    </Combobox.Popover>
+  </Combobox>
 )
 
 describe('Combobox component', () => {
