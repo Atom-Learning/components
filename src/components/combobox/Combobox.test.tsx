385971b42d2b17262df6613052c97b497c10c4a2
Gary Holland <gary@atomlearning.co.uk>
2021-04-28 11:41:29 +0100

    chore: test functionality

--- src/components/combobox/Combobox.test.tsx
+++ src/components/combobox/Combobox.test.tsx
@@ -1,19 +1,48 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import * as React from 'react'
 
+import { Label } from '../label'
 import { Combobox } from '.'
 
-describe("Combobox component", () => {
+const SimpleExample = () => (
+  <>
+    <Label htmlFor="fruit">What's your favourite fruit?</Label>
+    <Combobox openOnFocus>
+      <Combobox.Input id="fruit" />
+      <Combobox.Popover>
+        <Combobox.List>
+          <Combobox.Option value="Apple" />
+          <Combobox.Option value="Banana" />
+          <Combobox.Option value="Cranberry" />
+          <Combobox.Option value="Dragon fruit" />
+        </Combobox.List>
+      </Combobox.Popover>
+    </Combobox>
+  </>
+)
+
+describe('Combobox component', () => {
   it('renders', async () => {
-    const { container } = render(<Combobox />)
+    const { container } = render(<SimpleExample />)
 
     expect(container).toMatchSnapshot()
   })
 
   it('has no programmatically detectable a11y issues', async () => {
-    const { container } = render(<Combobox />)
+    const { container } = render(<SimpleExample />)
 
     expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('receives value from list on selection', async () => {
+    render(<SimpleExample />)
+
+    const combobox = screen.getByRole('combobox')
+
+    fireEvent.focus(combobox)
+    fireEvent.click(screen.getByText('Apple'))
+
+    expect(combobox).toHaveValue('Apple')
   })
 })
