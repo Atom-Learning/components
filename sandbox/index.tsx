6780c24ed4e9e980c8fc229f6287c130e7e5f820
Gary Holland <gary@atomlearning.co.uk>
2021-04-28 11:06:40 +0100

    chore: add combobox docs

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, Combobox, globalCss, Label, Button } from '../dist'
+import { Box, Combobox, globalCss, Label, Button, Input, Flex } from '../dist'
 
 globalCss(reset)()
 
@@ -18,13 +18,23 @@ const App = () => {
         <Combobox.Input id="someid" />
         <Combobox.Popover>
           <Combobox.List>
-            <Combobox.Option value="Apple">
-              nice nice <Combobox.OptionText />
-            </Combobox.Option>
+            <Combobox.Option value="Apple" />
             <Combobox.Option value="Banana" />
             <Combobox.Option value="Cranberry" />
             <Combobox.Option value="Dragon fruit" />
-            <Button>Do something</Button>
+
+            <Flex css={{ alignItems: 'center' }}>
+              <Input placeholder="New fruit" css={{ height: '$2' }} />
+              <Button
+                size="sm"
+                css={{ m: '$2' }}
+                onClick={() =>
+                  alert('Nest other interactive UI here for advanced usecases')
+                }
+              >
+                Add a new fruit
+              </Button>
+            </Flex>
           </Combobox.List>
         </Combobox.Popover>
       </Combobox>
