f0bb350dc0034a50f9c0171b900e312f3ab043e0
Gary Holland <gary@atomlearning.co.uk>
2021-04-28 16:37:19 +0100

    chore: simplify popover positioning

--- src/components/combobox/ComboboxPopover.tsx
+++ src/components/combobox/ComboboxPopover.tsx
@@ -9,15 +9,8 @@ export const ComboboxPopover = styled(BaseComboboxPopover, {
   boxShadow: '$0',
   boxSizing: 'border-box',
   fontFamily: '$sans',
-  fontSize: '$sm',
+  fontSize: '$md',
   outline: 'none',
   p: '$2',
-
-  // top and position are set by @reach/ui and
-  // override our styling without !important
-  // ... but !important causes a compilation error on position!
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  position: 'relative !important',
-  top: '$1 !important'
+  transform: 'translateY(4px)'
 })
