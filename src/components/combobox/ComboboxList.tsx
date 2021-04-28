2989bc7575971001b6d76ac5c38667ca91299206
Gary Holland <gary@atomlearning.co.uk>
2021-04-28 16:04:23 +0100

    chore: reorganise styles

--- src/components/combobox/ComboboxList.tsx
+++ src/components/combobox/ComboboxList.tsx
@@ -7,10 +7,4 @@ export const ComboboxList = styled(BaseComboboxList, {
   margin: 0,
   padding: 0,
   userSelect: 'none'
-  // '&[data-reach-combobox-list]': {
-  //   listStyle: 'none',
-  //   margin: 0,
-  //   padding: 0,
-  //   userSelect: 'none'
-  // }
 })
--- src/components/combobox/ComboboxPopover.tsx
+++ src/components/combobox/ComboboxPopover.tsx
@@ -3,20 +3,21 @@ import { ComboboxPopover as BaseComboboxPopover } from '@reach/combobox'
 import { styled } from '~/stitches'
 
 export const ComboboxPopover = styled(BaseComboboxPopover, {
+  background: 'white',
+  border: 'solid 1px $tonal500',
   borderRadius: '$0',
   boxShadow: '$0',
-  outline: 'none',
   boxSizing: 'border-box',
   fontFamily: '$sans',
   fontSize: '$sm',
+  outline: 'none',
   p: '$2',
-  border: 'solid 1px $tonal500',
-  background: 'white',
+
   // top and position are set by @reach/ui and
   // override our styling without !important
-  top: '$1 !important',
   // ... but !important causes a compilation error on position!
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  position: 'relative !important'
+  position: 'relative !important',
+  top: '$1 !important'
 })
