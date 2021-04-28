07064f4741347771dcb743853dd630e1716409b1
Gary Holland <gary@atomlearning.co.uk>
2021-04-28 12:07:09 +0100

    chore: remove changes to sandbox

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -4,29 +4,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss, Label, Combobox } from '../dist'
+import { Box, globalCss } from '../dist'
 
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Box css={{ mx: 'auto', width: '400px', p: '$2' }}>
-      <Label htmlFor="fruit" css={{ mb: '$2' }}>
-        What's your favourite fruit?
-      </Label>
-      <Combobox openOnFocus>
-        <Combobox.Input id="fruit" />
-        <Combobox.Popover>
-          <Combobox.List>
-            <Combobox.Option value="Apple" />
-            <Combobox.Option value="Banana" />
-            <Combobox.Option value="Cranberry" />
-            <Combobox.Option value="Dragon fruit" />
-          </Combobox.List>
-        </Combobox.Popover>
-      </Combobox>
-    </Box>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
