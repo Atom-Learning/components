24b728fe161b559f44d131e6a9fb5aeee10e8602
Tom Digby <tom@atomlearning.co.uk>
2021-05-05 21:29:27 +0100

    feat: more work on date-picker

--- src/components/date-picker/DatePicker.tsx
+++ src/components/date-picker/DatePicker.tsx
@@ -1,14 +1,41 @@
+import { CalendarEvent, Close } from '@atom-learning/icons'
 import * as React from 'react'
 import ReactDatePicker from 'react-date-picker/dist/entry.nostyle'
 
 import { styled } from '~/stitches'
 
 const StyledDatePicker = styled(ReactDatePicker, {
+  '& .react-date-picker__wrapper': {
+    display: 'flex'
+  },
   '& .react-calendar__navigation__arrow': {
     border: 'unset',
     backgroundColor: 'unset',
     fontSize: '$md',
     size: '$3'
+  },
+  '& .react-date-picker__clear-button': {
+    mr: '$2'
+  },
+  '& .react-date-picker__button': {
+    bg: '$tonal200',
+    cursor: 'pointer',
+    border: 'unset',
+    size: '$4',
+    p: 'unset',
+    borderRadius: '$0',
+    transition: 'background-color 75ms ease-out',
+    '&:hover': {
+      bg: '$tonal300'
+    }
+  },
+  '& .react-date-picker__button svg': {
+    size: '$1',
+    fill: 'none',
+    stroke: 'currentcolor',
+    strokeLinecap: 'round',
+    strokeLinejoin: 'round',
+    strokeWidth: '2'
   },
   '& .react-calendar__navigation__label': {
     bg: 'unset',
@@ -20,9 +47,17 @@ const StyledDatePicker = styled(ReactDatePicker, {
   '& .react-date-picker__inputGroup__divider': {
     display: 'none'
   },
+  '& .react-date-picker__inputGroup__day': {
+    minWidth: 80
+  },
+  '& .react-date-picker__inputGroup__month': {
+    minWidth: 80
+  },
+  '& .react-date-picker__inputGroup__year': {
+    minWidth: 100
+  },
   '& .react-date-picker__inputGroup__input': {
     '-moz-appearance': 'textfield',
-    minWidth: 60,
     border: '1px solid $tonal500',
     borderRadius: '$0',
     boxShadow: 'none', // prevent default iOS default styling
@@ -34,6 +69,7 @@ const StyledDatePicker = styled(ReactDatePicker, {
     height: '$4',
     width: '100%',
     p: '$3 $2',
+    mr: '$2',
     transition: 'all 100ms ease-out',
     '&:focus': {
       borderColor: '$primary900',
@@ -44,9 +80,6 @@ const StyledDatePicker = styled(ReactDatePicker, {
       backgroundColor: '$tonal300',
       color: '$tonal700',
       cursor: 'not-allowed'
-    },
-    '&:not(:last-child)': {
-      mr: '$2'
     }
   },
   '& .react-date-picker__calendar': {
@@ -55,42 +88,56 @@ const StyledDatePicker = styled(ReactDatePicker, {
     bg: 'white',
     borderRadius: '$0',
     p: '$3',
-    width: 220,
+    width: 244,
     opacity: 1,
     visibility: 'visible',
-    transition: 'all 75ms ease-out'
+    transition: 'all 75ms ease-out',
+    transform: 'translateY($space$3)'
   },
   '&.react-date-picker--closed .react-date-picker__calendar': {
     opacity: 0,
     visibility: 'hidden'
   },
   '& .react-calendar__month-view__weekdays': {
-    fontSize: '$sm',
+    fontSize: '$xs',
     fontFamily: '$sans',
     py: '$2'
   },
   '& .react-calendar__tile': {
     bg: 'unset',
     height: '$2',
-    border: 'unset'
+    border: 'unset',
+    cursor: 'pointer',
+    transition: 'all 75ms ease-out',
+    '&:hover': {
+      bg: '$tonal200'
+    }
   },
   '& .react-calendar__month-view__days__day--neighboringMonth': {
     color: '$tonal400'
   },
   '& .react-calendar__tile--active': {
-    bg: '$tonal400'
+    bg: '$tonal200'
   }
 })
 
 type DatePickerProps = {
-  date?: Date
+  date: Date
 }
 
-export const DatePicker: React.FC<DatePickerProps> = ({
-  date = new Date()
-}) => {
+export const DatePicker: React.FC<DatePickerProps> = ({ date }) => {
+  const [value, setValue] = React.useState<Date | Date[]>(date)
+
   return (
-    <StyledDatePicker onChange={(value) => console.log(value)} value={date} />
+    <StyledDatePicker
+      clearIcon={<Close />}
+      calendarIcon={<CalendarEvent />}
+      onChange={(val) => setValue(val)}
+      value={value}
+      yearPlaceholder="Year"
+      dayPlaceholder="Day"
+      monthPlaceholder="Month"
+    />
   )
 }
 
