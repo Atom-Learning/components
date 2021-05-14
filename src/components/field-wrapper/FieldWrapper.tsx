af17e22995b69610bc26ad1eb08d9559d53c47df
Tom Digby <tom@atomlearning.co.uk>
2021-05-14 15:13:40 +0100

    fix: remove field wrapper spacing update

--- src/components/field-wrapper/FieldWrapper.tsx
+++ src/components/field-wrapper/FieldWrapper.tsx
@@ -31,7 +31,7 @@ export const FieldWrapper: React.FC<FieldWrapperProps> = ({
         css={{
           justifyContent: 'space-between',
           alignItems: 'center',
-          mb: '$3'
+          mb: '$2'
         }}
       >
         <Label htmlFor={fieldId} required={required}>
