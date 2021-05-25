4bb47cbf77a4baa921cd54fb0bd50624837f85fa
Gary Holland <gary@atomlearning.co.uk>
2021-05-25 12:58:17 +0100

    chore: remove sandbox changes

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,25 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, Button, Form, globalCss, InputField,PasswordField } from '../src'
+import { Box, globalCss } from '../src'
 
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Form onSubmit={console.log} css={{ width: 500 }}>
-      <InputField
-        name="name"
-        label="Name"
-        validation={{ required: 'Name is required' }}
-      />
-      <PasswordField
-        name="password"
-        validation={{ required: 'Password is required' }}
-      />
-      <Button type="submit">Submit</Button>
-    </Form>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
