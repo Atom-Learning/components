281357f16315c5c817ed47a1e055afbd358d3029
Gary Holland <gary@atomlearning.co.uk>
2021-05-25 12:59:38 +0100

    chore: remove unneeded changes

--- src/components/form/Form.test.tsx
+++ src/components/form/Form.test.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { axe } from 'jest-axe'
 import * as React from 'react'
@@ -33,7 +33,7 @@ describe(`Form component`, () => {
 
   it('passes error messages to fields', async () => {
     render(
-      <Form onSubmit={jest.fn()} css={{ width: 500 }}>
+      <Form onSubmit={jest.fn()}>
         <InputField
           name="name"
           label="Name"
@@ -43,11 +43,13 @@ describe(`Form component`, () => {
           name="password"
           validation={{ required: 'Password is required' }}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" onClick={jest.fn()}>
+          Submit
+        </Button>
       </Form>
     )
 
-    await act(async () => userEvent.click(screen.getByText('Submit')))
+    userEvent.click(screen.getByText('Submit'))
 
     expect(await screen.findByText('Name is required'))
     expect(await screen.findByText('Password is required'))
@@ -66,7 +68,6 @@ describe(`Form component`, () => {
             />
             <PasswordField
               name="password"
-              label="Password"
               validation={{ required: 'Password is required' }}
             />
             <Button
