572bf6bc25aef9225ab3013b7026eb96354d77ef
Thomas Digby <tom@atomlearning.co.uk>
2021-05-25 15:42:49 +0100

    Update prevent-commit.sh

--- scripts/prevent-commit.sh
+++ scripts/prevent-commit.sh
@@ -2,6 +2,6 @@ SRC_PATTERN="sandbox/index.tsx"
 
 if git diff --cached --name-only | grep --quiet "$SRC_PATTERN"
 then
-  echo -e "\033[1;31mCommit failed. Do not make changes to $SRC_PATTERN\033[0m"
+  echo -e "\033[1;31mCommit failed. Please ensure that you do not commit changes to $SRC_PATTERN\033[0m"
   exit 1
 fi
