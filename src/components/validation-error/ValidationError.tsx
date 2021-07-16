95edb07ad6e7220ad44bc180268277aed2e93658
Tom Digby <tom@atomlearning.co.uk>
2021-07-16 15:48:02 +0100

    fix: validation style, reach ui console warning

--- src/components/validation-error/ValidationError.tsx
+++ src/components/validation-error/ValidationError.tsx
@@ -1,6 +1,7 @@
 import { Error } from '@atom-learning/icons'
 import * as React from 'react'
 
+import { Flex } from '~/components/flex'
 import { Icon } from '~/components/icon'
 import { Text } from '~/components/text'
 import { CSS } from '~/stitches'
@@ -14,17 +15,13 @@ export const ValidationError: React.FC<ValidationErrorProps> = ({
   children,
   ...remainingProps
 }) => (
-  <Text
-    css={{
-      color: '$danger',
-      display: 'flex',
-      alignItems: 'center',
-      ...(css as any)
-    }}
-    size="sm"
+  <Flex
+    css={{ color: '$danger', alignItems: 'center', ...(css as any) }}
     {...remainingProps}
   >
     <Icon is={Error} size="sm" css={{ mr: '$2' }} aria-hidden />
-    {children}
-  </Text>
+    <Text css={{ color: 'inherit' }} size="sm">
+      {children}
+    </Text>
+  </Flex>
 )
