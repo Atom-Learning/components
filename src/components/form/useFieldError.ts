0b35a79444f8372e86afc113cb8f1a1b9a44d486
Jakub Janicki <jakub.janicki@gmail.com>
2021-08-05 11:36:02 +0200

    fix: added a comment to the error hook in case of a v7 migration

--- src/components/form/useFieldError.ts
+++ src/components/form/useFieldError.ts
@@ -6,6 +6,7 @@ export const useFieldError = (fieldName: string) => {
   const [error, setError] = useState<string | undefined>(undefined)
   const { errors } = useFormContext()
 
+  // in case of migration to a newer version of react-hook-form the path needs to updated to account for dynamic fields naming convention
   const fieldPath = fieldName.split(/[,[\].]+?/).filter(Boolean)
 
   const getErrors = () => delve(errors, fieldPath)
