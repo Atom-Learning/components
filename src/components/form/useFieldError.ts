a83b510d00f7c82470afb3c607069d6ae375e23a
Jakub Janicki <jakub.janicki@gmail.com>
2021-08-05 11:39:55 +0200

    fix: removed useFieldError unnecessary local state

--- src/components/form/useFieldError.ts
+++ src/components/form/useFieldError.ts
@@ -1,26 +1,15 @@
-import { useState, useEffect } from 'react'
 import delve from 'dlv'
 import { useFormContext } from 'react-hook-form'
 
 export const useFieldError = (fieldName: string) => {
-  const [error, setError] = useState<string | undefined>(undefined)
   const { errors } = useFormContext()
 
   // in case of migration to a newer version of react-hook-form the path needs to updated to account for dynamic fields naming convention
   const fieldPath = fieldName.split(/[,[\].]+?/).filter(Boolean)
 
   const getErrors = () => delve(errors, fieldPath)
-  const currentFieldError = getErrors()
-
-  useEffect(() => {
-    if (currentFieldError) {
-      setError(currentFieldError.message)
-    } else {
-      setError(undefined)
-    }
-  }, [currentFieldError])
 
   return {
-    error
+    error: getErrors()?.message
   }
 }
