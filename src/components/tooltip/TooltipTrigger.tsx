d101afdcf9fc5f82c2c0ef5056f7812a4a095870
Gary Holland <gary@atomlearning.co.uk>
2021-10-06 11:44:08 +0100

    chore: re-export tooltip trigger

--- src/components/tooltip/TooltipTrigger.tsx
+++
@@ -1,9 +0,0 @@
-import { Trigger } from '@radix-ui/react-tooltip'
-import * as React from 'react'
-
-type TooltipTriggerProps = React.ComponentProps<typeof Trigger>
-
-export const TooltipTrigger: React.FC<TooltipTriggerProps> = ({
-  children,
-  ...remainingProps
-}) => <Trigger {...remainingProps}>{children}</Trigger>
