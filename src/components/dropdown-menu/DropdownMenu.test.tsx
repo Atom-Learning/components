c97610f6516ebe78e613289535b137ec7eb17735
Gary Holland <gary@atomlearning.co.uk>
2021-10-07 16:26:25 +0100

    feat: dropdown tests

---
+++ src/components/dropdown-menu/DropdownMenu.test.tsx
@@ -0,0 +1,49 @@
+import { IdProvider } from '@radix-ui/react-id'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { axe } from 'jest-axe'
+import * as React from 'react'
+
+import { DropdownMenu } from '.'
+
+const ExampleDropdownMenu = () => (
+  <IdProvider>
+    <DropdownMenu>
+      <DropdownMenu.Trigger>TRIGGER</DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        <DropdownMenu.Item>Item 1</DropdownMenu.Item>
+        <DropdownMenu.Item>Item 2</DropdownMenu.Item>
+        <DropdownMenu.Separator />
+        <DropdownMenu.LinkItem href="/logout">Log Out</DropdownMenu.LinkItem>
+      </DropdownMenu.Content>
+    </DropdownMenu>
+  </IdProvider>
+)
+
+describe('FieldWrapper component', () => {
+  it('renders', async () => {
+    const { container } = render(<ExampleDropdownMenu />)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('has no programmatically detectable a11y issues', async () => {
+    const { container } = render(<ExampleDropdownMenu />)
+
+    expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('opens menu onClick of trigger', async () => {
+    render(<ExampleDropdownMenu />)
+
+    const trigger = screen.getByText('TRIGGER')
+
+    expect(await trigger).toBeInTheDocument()
+
+    expect(await screen.queryByText('Item 1')).not.toBeInTheDocument()
+    userEvent.click(trigger)
+    waitFor(async () =>
+      expect(await screen.queryByText('Item 1')).toBeInTheDocument()
+    )
+  })
+})
--- src/components/dropdown-menu/DropdownMenuLinkItem.tsx
+++ src/components/dropdown-menu/DropdownMenuLinkItem.tsx
@@ -5,16 +5,13 @@ import { styled } from '~/stitches'
 import { DropdownMenuItem } from './DropdownMenuItem'
 
 const StyledLink = styled('a', {
-  alignItems: 'center',
-  color: 'currentColor',
-  display: 'flex',
   textDecoration: 'none'
 })
 
 export const DropdownMenuLinkItem: React.FC<
   React.ComponentProps<typeof DropdownMenuItem> & { href: string }
 > = ({ children, href, ...props }) => (
-  <DropdownMenuItem {...props} role="presentation">
+  <DropdownMenuItem {...props} asChild>
     <StyledLink href={href} role="menuitem">
       {children}
     </StyledLink>
