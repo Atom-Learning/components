d5901ab6b708b6bcf4bfc6e2f9b1a9fab445fe76
Gary Holland <gary@atomlearning.co.uk>
2021-10-07 12:43:08 +0100

    chore: merge

--- sandbox/index.tsx
+++ sandbox/index.tsx
@@ -2,36 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
 
-import { Box, globalCss, Button, DropdownMenu, Icon } from '../src'
-import { Error, Exit, User } from '@atom-learning/icons'
+import { Box } from '../src'
 
 globalCss(reset)()
 
 const App = () => {
-  return (
-    <Box css={{ height: 200, width: '500px', mx: 'auto', p: '$4' }}>
-      <DropdownMenu>
-        <DropdownMenu.Trigger asChild>
-          <Button>click click</Button>
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content sideOffset={20}>
-          <DropdownMenu.Item active>
-            <Icon is={User} css={{ mr: '$2' }} />
-            Profile
-          </DropdownMenu.Item>
-          <DropdownMenu.Item>
-            <Icon is={Error} css={{ mr: '$2' }} />
-            Report a Problem
-          </DropdownMenu.Item>
-          <DropdownMenu.Separator />
-          <DropdownMenu.Item>
-            <Icon is={Exit} css={{ mr: '$2' }} />
-            Log Out
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu>
-    </Box>
-  )
+  return <Box />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
