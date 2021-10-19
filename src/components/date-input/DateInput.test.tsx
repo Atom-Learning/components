af59ab1e77377fac7c71f7e785f44a81b3e24650
Tom Digby <tom@atomlearning.co.uk>
2021-10-19 16:13:34 +0100

    fix: update snaps

--- src/components/date-input/DateInput.test.tsx
+++ src/components/date-input/DateInput.test.tsx
@@ -1,12 +1,17 @@
+import { IdProvider } from '@radix-ui/react-id'
 import { render } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import * as React from 'react'
 
 import { DateInput } from '.'
 
-describe("DateInput component", () => {
+describe('DateInput component', () => {
   it('renders', async () => {
-    const { container } = render(<DateInput />)
+    const { container } = render(
+      <IdProvider>
+        <DateInput />
+      </IdProvider>
+    )
 
     expect(container).toMatchSnapshot()
   })
