5948e80a95f813bacd9d6f352e1f040ee2b2c802
Avinash Verma <avinash.verma@atomlearning.co.uk>
2021-10-20 14:48:10 +0100

    feat: enhance Divider component to enable horizontal and vertical orientation

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -1,8 +1,29 @@
-import { styled } from '~/stitches'
+import React from 'react'
 
-export const Divider = styled('hr', {
-  border: 0,
-  borderTop: '1px solid $tonal200',
-  mx: 'auto',
-  width: '50%'
+import { CSS, styled } from '~/stitches'
+
+interface DividerProps {
+  orientation?: 'horizontal' | 'vertical'
+  css?: CSS
+}
+
+const DEFAULT_ORIENTATION: DividerProps['orientation'] = 'horizontal'
+
+const StyledDivider = styled('div', {
+  bg: '$tonal200',
+  '&[data-orientation=horizontal]': { height: 1, width: '100%' },
+  '&[data-orientation=vertical]': { height: '100%', width: 1 }
 })
+
+export const Divider: React.FC<DividerProps> = ({
+  orientation = DEFAULT_ORIENTATION,
+  css
+}) => {
+  return (
+    <StyledDivider
+      data-orientation={orientation}
+      aria-orientation={orientation}
+      css={css}
+    />
+  )
+}
