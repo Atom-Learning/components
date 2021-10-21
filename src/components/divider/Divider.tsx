a95bb772e5325c0dffab3a3793f17f70210c0fdd
Avinash Verma <avinash.verma@atomlearning.co.uk>
2021-10-21 12:39:20 +0100

    chore: add a minimum height for vertical oriented divider

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -12,7 +12,7 @@ const DEFAULT_ORIENTATION: DividerProps['orientation'] = 'horizontal'
 const StyledDivider = styled('div', {
   bg: '$tonal200',
   '&[data-orientation=horizontal]': { height: 1, width: '100%' },
-  '&[data-orientation=vertical]': { height: '100%', width: 1 }
+  '&[data-orientation=vertical]': { height: '100%', width: 1, minHeight: '$3' }
 })
 
 export const Divider: React.FC<DividerProps> = ({
