cec5b04235f2c24915050dfd71975608ddee7c86
Avinash Verma <avinash.verma@atomlearning.co.uk>
2021-11-18 05:52:05 +0000

    feat: add tab scrollers which allows easy navigation if tabs length is more than available space

--- src/components/tabs/Tabs.tsx
+++ src/components/tabs/Tabs.tsx
@@ -1,9 +1,15 @@
 import { Content, List, Root } from '@radix-ui/react-tabs'
-import * as React from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
+import { ChevronLeft, ChevronRight } from '@atom-learning/icons'
 
 import { styled } from '~/stitches'
+import { ActionIcon } from '~/components/action-icon'
+import { Flex } from '~/components/flex'
+import { Icon } from '~/components/icon'
 
 import { TabTrigger } from './TabTrigger'
+
+type TabsProps = React.ComponentProps<typeof StyledRoot>
 
 const StyledRoot = styled(Root, {
   display: 'flex',
@@ -20,17 +26,106 @@ const StyledTriggerList = styled(List, {
   borderBottom: '1px solid $primaryDark'
 })
 
-type TabsProps = React.ComponentProps<typeof StyledRoot>
+const StyledActionIcon = styled(ActionIcon, {
+  position: 'absolute',
+  top: 'calc(50% - 20px)',
+  bg: 'white !important',
+  opacity: 0.8
+})
+
+type ListProps = React.ComponentProps<typeof StyledTriggerList> & {
+  enableTabScrolling?: boolean
+}
+
+const TriggerListWrapper: React.FC<ListProps> = ({
+  css,
+  enableTabScrolling,
+  ...rest
+}) => {
+  const triggerListRef = useRef<HTMLDivElement>(null)
+  const [showScroller, setShowScroller] = useState<boolean>(false)
+
+  const scrollTriggerListTo = useCallback((direction: 'left' | 'right') => {
+    const triggerList = triggerListRef.current
+    if (triggerList) {
+      const { scrollWidth } = triggerList
+      const scrollAmount = scrollWidth / 4 // 25% of whole scroll width
+      if (direction === 'right') {
+        if (triggerList.scrollLeft + scrollAmount <= scrollWidth) {
+          triggerList.scroll({
+            left: triggerList.scrollLeft + scrollAmount,
+            behavior: 'smooth'
+          })
+        }
+      } else {
+        triggerList.scroll({
+          left:
+            triggerList.scrollLeft - scrollAmount > 0
+              ? triggerList.scrollLeft - scrollAmount
+              : 0,
+          behavior: 'smooth'
+        })
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    const triggerList = triggerListRef.current
+    if (triggerList) {
+      const { offsetWidth, scrollWidth } = triggerList
+
+      const shouldShowScroller = scrollWidth > offsetWidth
+      if (showScroller !== shouldShowScroller) {
+        setShowScroller(shouldShowScroller)
+      }
+    }
+  }, [])
+
+  return showScroller || enableTabScrolling ? (
+    <Flex css={{ position: 'relative' }}>
+      <StyledActionIcon
+        size="lg"
+        label="scroll-left"
+        onClick={() => scrollTriggerListTo('left')}
+        css={{ left: 0 }}
+      >
+        <Icon is={ChevronLeft} size="lg" />
+      </StyledActionIcon>
+      <StyledTriggerList
+        {...rest}
+        ref={triggerListRef}
+        css={{
+          ...css,
+          width: '100%',
+          overflowX: 'auto',
+          '&::-webkit-scrollbar': { display: 'none' },
+          'div:first-child': { pl: '$6' },
+          'div:last-child': { pr: '$6' }
+        }}
+      />
+      <StyledActionIcon
+        size="lg"
+        label="scroll-right"
+        onClick={() => scrollTriggerListTo('right')}
+        css={{ right: 0 }}
+      >
+        <Icon is={ChevronRight} size="lg" />
+      </StyledActionIcon>
+    </Flex>
+  ) : (
+    <StyledTriggerList css={css} {...rest} ref={triggerListRef} />
+  )
+}
 
 export const Tabs: React.FC<TabsProps> & {
-  TriggerList: typeof StyledTriggerList
+  TriggerList: typeof TriggerListWrapper
   Trigger: typeof TabTrigger
   Content: typeof StyledTabContent
 } = ({ children, ...remainingProps }) => (
   <StyledRoot {...remainingProps}>{children}</StyledRoot>
 )
 
-Tabs.TriggerList = StyledTriggerList
+Tabs.TriggerList = TriggerListWrapper
 Tabs.Trigger = TabTrigger
 Tabs.Content = StyledTabContent
 
