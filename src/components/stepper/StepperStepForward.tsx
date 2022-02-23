f7e1c208dbb911ccaade2bebb6cdbf49fe82f39b
Alexandru Nemes <alexandru.nemes89@gmail.com>
2022-02-23 15:42:12 +0200

    fix: stepper types

--- src/components/stepper/StepperStepForward.tsx
+++ src/components/stepper/StepperStepForward.tsx
@@ -5,13 +5,13 @@ import { useStepper } from './stepper-context/StepperContext'
 import { IStepperNavigateProps } from './types'
 
 export const StepperStepForward: React.FC<
-  IStepperNavigateProps & React.ComponentProps<typeof Button>
+  IStepperNavigateProps & Partial<React.ComponentProps<typeof Button>>
 > = ({ label, children, ...rest }) => {
   const { goToNextStep, activeStep } = useStepper()
 
   return (
     <Button size="sm" {...rest} onClick={goToNextStep} css={{ ml: 'auto' }}>
-      {children || label(activeStep)}
+      {children || label?.(activeStep)}
     </Button>
   )
 }
