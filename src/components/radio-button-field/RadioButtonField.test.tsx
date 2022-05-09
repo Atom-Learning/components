4ddb2629673367deb382ecae8e5e991c1a6d40b2
Marcdup1 <marc.duplessis1@gmail.com>
2022-05-09 11:03:29 +0200

    fix: allow on change handler in radio button field

--- src/components/radio-button-field/RadioButtonField.test.tsx
+++ src/components/radio-button-field/RadioButtonField.test.tsx
@@ -1,15 +1,25 @@
 import { IdProvider } from '@radix-ui/react-id'
-import { render } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { axe } from 'jest-axe'
 import * as React from 'react'
 
 import { Form } from '../'
 import { RadioButtonField } from '.'
 
-const ExampleRadioButtonField = () => (
+type ExampleRadioFieldProps = Omit<
+  React.ComponentProps<typeof RadioButtonField>,
+  'name' | 'label'
+>
+
+const ExampleRadioButtonField = (props: ExampleRadioFieldProps) => (
   <IdProvider>
     <Form>
-      <RadioButtonField name="example" defaultValue="1" label="Example options">
+      <RadioButtonField
+        name="example"
+        defaultValue="1"
+        label="Example options"
+        {...props}
+      >
         <RadioButtonField.Item label="1" value="1" />
         <RadioButtonField.Item label="2" value="2" />
       </RadioButtonField>
@@ -30,5 +40,17 @@ describe('RadioButtonField component', () => {
     const { container } = render(<ExampleRadioButtonField />)
 
     expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('allows an external on value change handler to be passed to the component', async () => {
+    const onValueChangeSpy = jest.fn()
+    render(<ExampleRadioButtonField onValueChange={onValueChangeSpy} />)
+
+    const button = await screen.findByLabelText('2')
+    fireEvent.click(button)
+    await waitFor(() => {
+      expect(onValueChangeSpy).toHaveBeenCalledTimes(1)
+      expect(onValueChangeSpy).toHaveBeenCalledWith('2')
+    })
   })
 })
