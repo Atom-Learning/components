d29f460e42b0e5f7fd921715b371738b29c1d4ac
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2022-05-20 12:21:04 +0000

    test: add tests for badge

--- src/components/badge/Badge.test.tsx
+++ src/components/badge/Badge.test.tsx
@@ -6,14 +6,24 @@ import { Badge } from '.'
 
 describe('Badge component', () => {
   it('renders', async () => {
-    const { container } = render(<Badge />)
+    const { container } = render(<Badge>Some Text</Badge>)
 
     expect(container).toMatchSnapshot()
   })
 
   it('has no programmatically detectable a11y issues', async () => {
-    const { container } = render(<Badge />)
+    const { container } = render(<Badge>Some Text</Badge>)
 
     expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('renders a md size and danger theme', async () => {
+    const { container } = render(
+      <Badge theme="danger" size="md">
+        Some Text
+      </Badge>
+    )
+
+    expect(container).toMatchSnapshot()
   })
 })
