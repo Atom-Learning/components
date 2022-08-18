b7f4a0e5034fdefae277d7b7855b60130f342f56
Vinicius Volpe <viniciusvolpe@macbook-pro.home>
2022-08-18 09:49:15 +0100

    feat: creating bulk actions bar component

---
+++ lib/src/components/buld-actions-bar/BulkAction.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+
+import { Button } from '../button'
+import { DropdownMenu } from '../dropdown-menu'
+import { Icon } from '../icon/Icon'
+
+type BulkActionProps = {
+  isMain?: boolean
+  icon?: React.FC<React.SVGProps<SVGSVGElement>>
+  text: string
+} & React.ComponentProps<typeof DropdownMenu.Item & typeof Button>
+
+export const BulkAction: React.FC<BulkActionProps> = ({
+  isMain,
+  icon,
+  text,
+  ...rest
+}) => {
+  const iconComponent = icon ? (
+    <Icon is={icon} size="sm" css={{ mr: '$2' }} />
+  ) : null
+  const { css, ...otherProps } = rest
+  return isMain ? (
+    <Button
+      appearance="outline"
+      size="sm"
+      css={{
+        border: 'none',
+        '@media (max-width: 640px)': {
+          display: 'none'
+        },
+        ...css
+      }}
+      {...otherProps}
+    >
+      {iconComponent}
+      {text}
+    </Button>
+  ) : (
+    <DropdownMenu.Item {...rest}>
+      {iconComponent}
+      {text}
+    </DropdownMenu.Item>
+  )
+}
+
+BulkAction.displayName = 'BulkAction'
---
+++ lib/src/components/buld-actions-bar/BulkActionsBar.tsx
@@ -0,0 +1,142 @@
+import { Close, Ellypsis } from '@atom-learning/icons'
+import * as React from 'react'
+import invariant from 'invariant'
+
+import { styled } from '../../stitches'
+
+import { ActionIcon } from '../action-icon'
+import { Divider } from '../divider'
+import { DropdownMenu } from '../dropdown-menu'
+import { Flex } from '../flex'
+import { Icon } from '../icon/Icon'
+import { Stack } from '../stack'
+import { Text } from '../text'
+import { BulkAction } from './BulkAction'
+import { Box } from '../box'
+
+const StyledContainer = styled(Flex, {
+  minHeight: '56px',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexWrap: 'nowrap',
+  bg: '$backgroundAccent',
+  p: '$2 $4',
+  borderRadius: '$0 $0 0 0'
+})
+
+type BulkActionsBarProps = {
+  label: string
+  cancelLabel: string
+  onCancel: () => void
+} & React.ComponentProps<typeof StyledContainer>
+
+export const BulkActionsBar: React.FC<BulkActionsBarProps> & {
+  Action: typeof BulkAction
+} = ({ label, cancelLabel, onCancel, children, ...rest }) => {
+  const mainActions = []
+  const otherActions = []
+
+  invariant(
+    React.Children.count(children) > 0,
+    `At least one child is required for ${BulkActionsBar.displayName}`
+  )
+
+  React.Children.forEach(children, (child) => {
+    if (!React.isValidElement(child)) {
+      throw new Error('Ivalid child element')
+    }
+
+    invariant(
+      child.type === BulkAction,
+      `Children of type ${child?.type.displayName} aren't permitted. Only an ${BulkAction.displayName} component is allowed in ${BulkActionsBar.displayName}`
+    )
+
+    if (child.props.isMain) {
+      mainActions.push(child)
+    } else {
+      otherActions.push(child)
+    }
+  })
+
+  const renderLargeScreenOptions = () => {
+    return otherActions.length ? (
+      <Box
+        css={{
+          '@media (max-width: 640px)': {
+            display: 'none'
+          }
+        }}
+      >
+        <DropdownMenu>
+          <DropdownMenu.Trigger asChild>
+            <ActionIcon>
+              <Icon is={Ellypsis} />
+            </ActionIcon>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>{otherActions}</DropdownMenu.Content>
+        </DropdownMenu>
+      </Box>
+    ) : null
+  }
+
+  const renderSmallScreenOptions = () => {
+    return otherActions.length || mainActions.length ? (
+      <Box
+        css={{
+          '@media (min-width: 640px)': {
+            display: 'none'
+          }
+        }}
+      >
+        <DropdownMenu>
+          <DropdownMenu.Trigger asChild>
+            <ActionIcon>
+              <Icon is={Ellypsis} />
+            </ActionIcon>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            {mainActions.map((action, index) =>
+              React.cloneElement(action, {
+                isMain: false,
+                key: `mainAction_${index}`
+              })
+            )}
+            {otherActions}
+          </DropdownMenu.Content>
+        </DropdownMenu>
+      </Box>
+    ) : null
+  }
+
+  return (
+    <StyledContainer {...rest}>
+      <Text size="sm">{label}</Text>
+      <Stack
+        css={{
+          '& :first-child': {
+            flexWrap: 'nowrap'
+          }
+        }}
+      >
+        {mainActions}
+        {renderLargeScreenOptions()}
+        {renderSmallScreenOptions()}
+        <Divider orientation="vertical" />
+        <BulkActionsBar.Action text={cancelLabel} onClick={onCancel} isMain />
+        <ActionIcon
+          onClick={onCancel}
+          css={{
+            '@media (min-width: 640px)': {
+              display: 'none'
+            }
+          }}
+        >
+          <Icon is={Close} />
+        </ActionIcon>
+      </Stack>
+    </StyledContainer>
+  )
+}
+
+BulkActionsBar.displayName = 'BulkActionsBar'
+BulkActionsBar.Action = BulkAction
