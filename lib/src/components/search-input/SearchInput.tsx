745c5bd06206abbc5afc64399be3a9b5ee3d3db9
kerryambt <kerry@getambt.co.uk>
2022-09-20 15:51:32 +0100

    feat: search field

--- lib/src/components/search-input/SearchInput.tsx
+++ lib/src/components/search-input/SearchInput.tsx
@@ -8,7 +8,7 @@ import { Input } from '~/components/input/'
 import { CSS, styled } from '~/stitches'
 import { useCallbackRef } from '~/utilities/hooks/useCallbackRef'
 
-type SearchInputProps = React.ComponentProps<typeof Input> & {
+export type SearchInputProps = React.ComponentProps<typeof Input> & {
   size: 'sm' | 'md'
   css?: CSS
   value?: string
@@ -48,82 +48,90 @@ const StyledSearchInput = styled(Input, {
     }
 })
 
-export const SearchInput: React.FC<SearchInputProps> = ({
-  size = 'md',
-  css,
-  value,
-  clearText = 'Clear',
-  onChange,
-  ...remainingProps
-}) => {
-  const [inputElRef, setInputElRef] = useCallbackRef()
-  const [inputValue, setInputValue] = React.useState<string | number>(
-    value || ''
-  )
-  const [activeIcon, setActiveIcon] = React.useState<INPUT_ICON>(
-    value ? INPUT_ICON.CLEAR : INPUT_ICON.SEARCH
-  )
+export const SearchInput: React.FC<SearchInputProps> = React.forwardRef(
+  (
+    {
+      size = 'md',
+      css,
+      value,
+      clearText = 'Clear',
+      onChange,
+      ...remainingProps
+    },
+    ref: (instance: HTMLInputElement | null) => void
+  ) => {
+    const [inputElRef] = useCallbackRef()
+    const [inputValue, setInputValue] = React.useState<string | number>(
+      value || ''
+    )
+    const [activeIcon, setActiveIcon] = React.useState<INPUT_ICON>(
+      value ? INPUT_ICON.CLEAR : INPUT_ICON.SEARCH
+    )
 
-  const handleClear = () => {
-    const inputEl = inputElRef.current
-    if (!inputEl) return
+    const handleClear = () => {
+      const inputEl = inputElRef.current
+      if (!inputEl) return
 
-    // https://stackoverflow.com/a/46012210
-    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
-      window.HTMLInputElement.prototype,
-      'value'
-    )?.set
-    nativeInputValueSetter?.call?.(inputEl, '')
-    const ev2 = new Event('input', {
-      bubbles: true
-    })
-    inputEl.dispatchEvent(ev2)
-    inputEl.focus()
-  }
+      // https://stackoverflow.com/a/46012210
+      const nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+        window.HTMLInputElement.prototype,
+        'value'
+      )?.set
+      nativeInputValueSetter?.call?.(inputEl, '')
+      const ev2 = new Event('input', {
+        bubbles: true
+      })
+      inputEl.dispatchEvent(ev2)
+      inputEl.focus()
+    }
 
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value)
-    setActiveIcon(event.target.value ? INPUT_ICON.CLEAR : INPUT_ICON.SEARCH)
-    onChange?.(event)
-  }
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInputValue(event.target.value)
+      setActiveIcon(event.target.value ? INPUT_ICON.CLEAR : INPUT_ICON.SEARCH)
+      onChange?.(event)
+    }
 
-  const getIcon = () => {
-    if (activeIcon === INPUT_ICON.SEARCH)
+    const getIcon = () => {
+      if (activeIcon === INPUT_ICON.SEARCH)
+        return (
+          <StyledIcon
+            is={Search}
+            size={size}
+            css={{ size: size == 'sm' ? '$1' : 20 }}
+          />
+        )
+
       return (
-        <StyledIcon
-          is={Search}
+        <ActionIcon
+          label={clearText}
+          theme="neutral"
           size={size}
-          css={{ size: size == 'sm' ? '$1' : 20 }}
-        />
+          css={{ position: 'absolute', top: '0', right: '$1' }}
+          onClick={handleClear}
+        >
+          <Icon is={Close} />
+        </ActionIcon>
       )
+    }
 
     return (
-      <ActionIcon
-        label={clearText}
-        theme="neutral"
-        size={size}
-        css={{ position: 'absolute', top: '0', right: '$1' }}
-        onClick={handleClear}
-      >
-        <Icon is={Close} />
-      </ActionIcon>
+      <Box css={{ position: 'relative', ...css }}>
+        <StyledSearchInput
+          ref={(e) => {
+            ref?.(e)
+            inputElRef.current = e
+          }}
+          size={size}
+          type="search"
+          {...remainingProps}
+          value={inputValue}
+          onChange={handleOnChange}
+          css={{ pr: size === 'sm' ? '$5' : '$6' }}
+        />
+        {getIcon()}
+      </Box>
     )
   }
-
-  return (
-    <Box css={{ position: 'relative', ...css }}>
-      <StyledSearchInput
-        ref={setInputElRef}
-        size={size}
-        type="search"
-        {...remainingProps}
-        value={inputValue}
-        onChange={handleOnChange}
-        css={{ pr: size === 'sm' ? '$5' : '$6' }}
-      />
-      {getIcon()}
-    </Box>
-  )
-}
+)
 
 SearchInput.displayName = 'SearchInput'
