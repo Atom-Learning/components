b591220ab07f8c87403207cbaad066eef9b0e983
Mzingaye Nkala <mzingaye.nkala@atomlearning.co.uk>
2022-09-28 12:16:58 +0000

    fix(TopBar): add size variant

--- lib/src/components/top-bar/TopBar.test.tsx
+++ lib/src/components/top-bar/TopBar.test.tsx
@@ -5,8 +5,8 @@ import * as React from 'react'
 
 import { TopBar } from '.'
 
-const ExampleTopBar = () => (
-  <TopBar>
+const ExampleTopBar = (size) => (
+  <TopBar size={size}>
     <TopBar.Brand href="atomlearning.co.uk">
       <TopBar.BrandName>Admin Panel</TopBar.BrandName>
     </TopBar.Brand>
@@ -17,7 +17,7 @@ const ExampleTopBar = () => (
 )
 
 describe('TopBar component', () => {
-  it('renders', async () => {
+  it('renders without size prop', async () => {
     const { container } = render(<ExampleTopBar />, {
       container: document.body
     })
@@ -25,8 +25,36 @@ describe('TopBar component', () => {
     expect(container).toMatchSnapshot()
   })
 
-  it('has no programmatically detectable a11y issues', async () => {
+  it('has no programmatically detectable a11y issues, when no size prop is passed', async () => {
     const { container } = render(<ExampleTopBar />)
+
+    expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('renders with the size prop md', async () => {
+    const { container } = render(<ExampleTopBar size="md" />, {
+      container: document.body
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('has no programmatically detectable a11y issues, when the size prop md is passed', async () => {
+    const { container } = render(<ExampleTopBar size="md" />)
+
+    expect(await axe(container)).toHaveNoViolations()
+  })
+
+  it('renders with the size prop lg', async () => {
+    const { container } = render(<ExampleTopBar size="lg" />, {
+      container: document.body
+    })
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('has no programmatically detectable a11y issues, when the size prop lg is passed', async () => {
+    const { container } = render(<ExampleTopBar size="lg" />)
 
     expect(await axe(container)).toHaveNoViolations()
   })
--- lib/src/components/top-bar/TopBar.tsx
+++ lib/src/components/top-bar/TopBar.tsx
@@ -27,7 +27,6 @@ const StyledRoot = styled('div', {
   alignItems: 'center',
   width: '100vw',
   top: '0',
-  height: '$6',
   zIndex: 1,
   borderBottom: '1px solid $tonal100',
   transition: 'box-shadow .2s ease-out',
@@ -36,30 +35,63 @@ const StyledRoot = styled('div', {
       true: {
         boxShadow: '0px 4px 4px -2px rgba(31, 31, 31, 0.1);'
       }
+    },
+    size: {
+      md: {
+        height: '$6'
+      },
+      lg: {
+        height: '$7'
+      }
     }
   }
 })
 
 const Container = styled(Flex, {
   alignItems: 'center',
-  height: '$4',
-  mx: '$4',
   width: '100%',
   '@md': {
     mx: '$5'
+  },
+  variants: {
+    size: {
+      md: {
+        height: '$4',
+        [`& ${TopBarBrandLogo}`]: {
+          '&[src$=".svg"]': {
+            height: 24,
+            width: 'auto'
+          }
+        }
+      },
+      lg: {
+        height: 80,
+        [`& ${TopBarBrandLogo}`]: {
+          '&[src$=".svg"]': {
+            height: 32,
+            width: 'auto'
+          },
+        }
+      }
+    }
   }
 })
 
-interface TopBarProps {
+type StyledRootProps = React.ComponentProps<typeof StyledRoot>
+
+interface TopBarProps extends StyledRootProps {
   css?: CSS
 }
 
-export const TopBar: React.FC<TopBarProps> & TopBarSubComponents = (props) => {
+export const TopBar: React.FC<TopBarProps> & TopBarSubComponents = ({
+  size = 'md',
+  ...props
+}) => {
   const { y: scrollPositionY } = useScrollPosition()
 
   return (
-    <StyledRoot hasScrolled={!!scrollPositionY}>
-      <Container {...props} />
+    <StyledRoot hasScrolled={!!scrollPositionY} size={size}>
+      <Container size={size} {...props} />
     </StyledRoot>
   )
 }
