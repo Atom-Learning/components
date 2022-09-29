a9bf934d6e35fa7ebf729f758bb573f0efd8bb43
Gary Holland <gary@atomlearning.co.uk>
2022-09-29 15:44:12 +0100

    fix: eslint-disable for fragment that eslint doesn't realise is v important

--- lib/src/utilities/optionally-visually-hidden-container/OptionallyVisuallyHiddenContainer.tsx
+++ lib/src/utilities/optionally-visually-hidden-container/OptionallyVisuallyHiddenContainer.tsx
@@ -6,5 +6,10 @@ export const OptionallyVisuallyHiddenContainer: React.FC<{
 }> = ({ children, hidden = false }) => {
   if (hidden) return <VisuallyHidden.Root>{children}</VisuallyHidden.Root>
 
-  return children ? <>{children}</> : null
+  return children ? (
+    // children could be multiple elements/components,
+    // so we need a fragment here.
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>{children}</>
+  ) : null
 }
