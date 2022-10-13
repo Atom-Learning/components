3b05a2cec8a725b99da67f5860ddd3324a411ad7
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2022-10-13 09:24:44 +0100

    wip

--- lib/jest-setup.ts
+++ lib/jest-setup.ts
@@ -3,3 +3,6 @@ import '@testing-library/jest-dom'
 import './test/mocks/ResizeObserver'
 import './test/mocks/DOMRect'
 import './test/patches/cssom'
+
+const { getComputedStyle } = window
+window.getComputedStyle = (elt) => getComputedStyle(elt)
