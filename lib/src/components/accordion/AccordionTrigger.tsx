75101879b477813106d5affc4d97ce04bb78c6f9
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2022-10-18 10:58:24 +0100

    feat: Accordion ColorScheme'd

--- lib/src/components/accordion/AccordionTrigger.tsx
+++ lib/src/components/accordion/AccordionTrigger.tsx
@@ -1,32 +1,13 @@
 import { ChevronDown } from '@atom-learning/icons'
 import { Trigger } from '@radix-ui/react-accordion'
-import { darken } from 'color2k'
+import { focusVisibleStyleBlock } from '~/utilities'
 import React from 'react'
 
-import { styled, theme } from '~/stitches'
+import { styled } from '~/stitches'
 
 import { Icon } from '../icon'
 
-const getTriggerVariant = (
-  base: string,
-  interact: string,
-  active: string,
-  text = '#fff'
-) => ({
-  bg: base,
-  color: text,
-  '&[disabled]': {
-    bg: '$tonal100',
-    cursor: 'not-allowed',
-    color: '$tonal400'
-  },
-  '&:not([disabled]):hover': {
-    bg: interact
-  },
-  '&:not([disabled]):active': {
-    bg: active
-  }
-})
+import { ColorScheme } from '@atom-learning/color-scheme'
 
 const RotatingIcon = styled(Icon, {
   transition: 'transform 300ms',
@@ -47,47 +28,41 @@ const StyledTrigger = styled(Trigger, {
   justifyContent: 'space-between',
   alignItems: 'center',
   cursor: 'pointer',
-
+  bg: '$interactive2',
+  color: '$interactiveForeground',
+  '&[data-disabled]': {
+    opacity: 0.3,
+    cursor: 'not-allowed'
+  },
+  '&:not([data-disabled])': {
+    '&:active, &:hover, &:focus-visible': {
+      bg: '$interactive3'
+    },
+    '&:focus-visible': {
+      ...focusVisibleStyleBlock(),
+    }
+  },
   '&[data-state="open"]': {
-    borderRadius: '$0 $0 0 0'
+    borderRadius: '$0 $0 0 0',
   },
   '&[data-state="closed"]': {
     borderRadius: '$0'
-  },
-
-  variants: {
-    theme: {
-      primaryDark: getTriggerVariant(
-        '$primaryDark',
-        darken(theme.colors.primaryDark.value, 0.1),
-        darken(theme.colors.primaryDark.value, 0.15)
-      ),
-      light: getTriggerVariant(
-        '#fff',
-        darken('#fff', 0.1),
-        darken('#fff', 0.15),
-        '$tonal600'
-      ),
-      tonal: getTriggerVariant(
-        '$tonal100',
-        darken('#fff', 0.1),
-        darken('#fff', 0.15),
-        '$tonal500'
-      )
-    }
   }
 })
 
-type AccordionTriggerProps = React.ComponentProps<typeof StyledTrigger>
+type AccordionTriggerProps = React.ComponentProps<typeof StyledTrigger> & {
+  color?: typeof ColorScheme.accent,
+  colorMode?: typeof ColorScheme.interactiveAccentMode,
+}
 
 export const AccordionTrigger: React.FC<AccordionTriggerProps> = ({
-  theme = 'primaryDark',
-
   children,
+  color = 'slate',
+  colorMode = 'hiContrast',
   ...remainingProps
 }) => (
-  <StyledTrigger theme={theme} {...remainingProps}>
+  <ColorScheme as={StyledTrigger} accent={color} interactiveAccentMode={colorMode} {...remainingProps}>
     {children}
     <RotatingIcon is={ChevronDown} />
-  </StyledTrigger>
+  </ColorScheme>
 )
