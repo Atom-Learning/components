02d8eefad59bbe82de8637f94897de5b2ea7e5d9
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2022-10-19 08:33:39 +0100

    fix: alert-dialog context change ci

--- lib/src/components/alert-dialog/alert-context/reducer.ts
+++ lib/src/components/alert-dialog/alert-context/reducer.ts
@@ -11,8 +11,7 @@ export const initialState = []
 export const reducer = (state: State, action: Action): alert[] => {
   switch (action.type) {
     case 'ADD':
-      const { id: payloadId, ...payloadRest } = action.payload
-      return [...state, { id: payloadId || uid(), ...payloadRest }] // @TODO
+      return [...state, { ...action.payload, id: action.payload.id || uid() }] // @TODO
     case 'REMOVE':
       return state.filter(({ id }) => id !== action.payload)
     default:
