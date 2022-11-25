ee8bed26fb9fd11a5b24c902cfeb25e2f4635a4c
Michal Rozenek <michalrozenek@MacBook-Pro-Michal.local>
2022-11-25 16:35:37 +0100

    feat: sidedrawer - overlay animation fix

--- lib/src/components/sidedrawer/SidedrawerOverlay.tsx
+++ lib/src/components/sidedrawer/SidedrawerOverlay.tsx
@@ -1,27 +1,28 @@
 import React from 'react'
 
 import { MAX_Z_INDEX } from '~/constants/zIndices'
+import { keyframes } from '~/stitches'
 
 import { Box } from '../box'
 
-const fadeIn = {
+const fadeIn = keyframes({
   from: {
     opacity: 0
   },
   to: {
-    opacity: 1
+    opacity: 0.6
   }
-}
+})
 
 export const SidedrawerOverlay: React.FC = () => (
   <Box
     css={{
-      animation: `${fadeIn} 100ms ease-out`,
+      animation: `${fadeIn} 300ms ease-in-out`,
+      animationFillMode: 'forwards',
       bg: '$tonal600',
       cursor: 'pointer',
       height: '100vh',
       left: '0',
-      opacity: '0.6',
       position: 'fixed',
       top: '0',
       width: '100vw',
