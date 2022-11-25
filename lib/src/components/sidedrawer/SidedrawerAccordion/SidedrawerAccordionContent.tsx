c6fa5bd227bbf748149f1cad2d4c1dc4d1ff0dee
Michal Rozenek <michalrozenek@MacBook-Pro-Michal.local>
2022-11-25 17:06:36 +0100

    feat: sidedrawer - accordtion item animation

--- lib/src/components/sidedrawer/SidedrawerAccordion/SidedrawerAccordionContent.tsx
+++ lib/src/components/sidedrawer/SidedrawerAccordion/SidedrawerAccordionContent.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import { Content } from '@radix-ui/react-accordion'
 import { Box } from '../../box/Box'
+import { keyframes, styled } from '~/stitches'
+
+const open = keyframes({
+  from: { height: 0 },
+  to: { height: 'var(--radix-accordion-content-height)' }
+})
+
+const close = keyframes({
+  from: { height: 'var(--radix-accordion-content-height)' },
+  to: { height: 0 }
+})
+
+const StyledContent = styled(Content, {
+  bg: 'white',
+  overflow: 'hidden',
+  '@allowMotion': {
+    '&[data-state="open"]': { animation: `${open} 300ms ease-out` },
+    '&[data-state="closed"]': { animation: `${close} 300ms ease-out` }
+  }
+})
 
 export const SidedrawerAccordionContent: React.FC = ({ children }) => (
-  <Content>
+  <StyledContent>
     {React.Children.map(children, (child) => {
       return (
         <Box
@@ -20,5 +40,5 @@ export const SidedrawerAccordionContent: React.FC = ({ children }) => (
         </Box>
       )
     })}
-  </Content>
+  </StyledContent>
 )
