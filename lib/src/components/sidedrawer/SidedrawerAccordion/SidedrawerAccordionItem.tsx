a989ba267f2a280d50262626189ae6b79836c4f0
Michal Rozenek <michalrozenek@MacBook-Pro-Michal.local>
2022-11-29 11:35:51 +0100

    feat: sidedrawer - cleanup accordion item render

--- lib/src/components/sidedrawer/SidedrawerAccordion/SidedrawerAccordionItem.tsx
+++ lib/src/components/sidedrawer/SidedrawerAccordion/SidedrawerAccordionItem.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Item } from '@radix-ui/react-accordion'
-import { SidedrawerAccordionTrigger } from './SidedrawerAccordionTrigger'
-import { SidedrawerAccordionContent } from './SidedrawerAccordionContent'
 
 type SidedrawerAccordionItemProps = React.ComponentProps<typeof Item>
 
@@ -9,19 +7,6 @@ export const SidedrawerAccordionItem: React.FC<
   SidedrawerAccordionItemProps
 > = ({ children, value, ...remainingProps }) => (
   <Item value={value} {...remainingProps}>
-    {React.Children.map(children, (child) => {
-      const isTrigger =
-        React.isValidElement(child) && child.type === SidedrawerAccordionTrigger
-      const isContent =
-        React.isValidElement(child) && child.type === SidedrawerAccordionContent
-
-      if (!isTrigger && !isContent) {
-        throw new Error(
-          'Only Sidedrawer.AccordionTrigger and Sidedrawer.AccordionContent can be the passed as a "children" of SidedrawerAccordionItem'
-        )
-      }
-
-      return child
-    })}
+    {children}
   </Item>
 )
