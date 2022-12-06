44464a99886a0e061d5a2a40022503c70758bbc5
Marcdup1 <marc.duplessis1@gmail.com>
2022-12-06 20:28:27 +0000

    refactor: remove extra styling from error component

--- lib/src/components/data-table/DataTableError.tsx
+++ lib/src/components/data-table/DataTableError.tsx
@@ -1,35 +1,18 @@
 import * as React from 'react'
 
-import { CSS } from '~/stitches'
-
-import { Flex } from '../flex'
 import { AsyncDataState, DataTableContextType } from './DataTable.types'
 import { useDataTable } from './DataTableContext'
 
 type TDataTableErrorProps = Omit<React.FC, 'children'> & {
-  css?: CSS
-  children: (retry: DataTableContextType['runAsyncData']) => React.ReactNode
+  children(retry: DataTableContextType['runAsyncData']): React.ReactElement
 }
 
 export const DataTableError: React.FC<TDataTableErrorProps> = ({
-  css,
   children
 }) => {
   const { asyncDataState, runAsyncData } = useDataTable()
 
   if (asyncDataState !== AsyncDataState.REJECTED) return null
 
-  return (
-    <Flex
-      css={{
-        flexDirection: 'column',
-        alignItems: 'center',
-        p: '$5',
-        bg: '$background',
-        ...css
-      }}
-    >
-      {children(runAsyncData)}
-    </Flex>
-  )
+  return children(runAsyncData)
 }
