22c2d1ecf5669095d26a9c84f6799f9f48cdabb1
Gary Holland <gary@atomlearning.co.uk>
2023-01-13 12:33:11 +0000

    chore: remove dupe dependency

--- lib/sandbox/index.tsx
+++ lib/sandbox/index.tsx
@@ -1,65 +1,22 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { reset } from 'stitches-reset'
-import { createColumnHelper } from '@tanstack/react-table'
-import { Box, DataTable, Flex, globalCss, Tooltip } from '../src'
+
+import { Box, Flex, globalCss } from '../src'
 
 globalCss({ ...reset, '*': { boxSizing: 'border-box' } })()
 
-const columnHelper = createColumnHelper<{
-  name: string
-  hobby: string
-}>()
-
-const columns = [
-  columnHelper.accessor('name', {
-    cell: (info) => info.getValue()
-  }),
-  columnHelper.accessor('hobby', {
-    cell: (info) => info.getValue()
-  }),
-  // Columns created with columnHelper.display won't be sortable.
-  // They need a header to be set manually since they're not just reading
-  // a property from the row.
-  columnHelper.display({
-    cell: (info) => <button>do something</button>,
-    header: 'Actions'
-  })
-]
-
-const data = [
-  { name: 'agatha', hobby: 'crossfit', id: 1 },
-  { name: 'betty', hobby: 'acting', id: 2 },
-  { name: 'chrissy', hobby: 'bare-knuckle boxing', id: 3 }
-]
-
 const App = () => (
-  <Tooltip.Provider>
-    <Flex
-      css={{
-        minHeight: '100vh',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-      }}
-    >
-      <Box css={{ height: '300px', width: '700px' }}>
-        <DataTable columns={columns} data={data}>
-          {/* <DataTable.GlobalFilter /> */}
-          <DataTable.DragAndDropContainer
-            onChange={(oldIndex, newIndex, newData) => {
-              console.log('old index:', oldIndex)
-              console.log('new index:', newIndex)
-              console.log('newData:', newData)
-            }}
-          >
-            <DataTable.Table sortable css={{ mb: '$4' }} />
-          </DataTable.DragAndDropContainer>
-          <DataTable.Pagination pageSize={5} />
-        </DataTable>
-      </Box>
-    </Flex>
-  </Tooltip.Provider>
+  <Flex
+    css={{
+      minHeight: '100vh',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }}
+  >
+    <Box />
+  </Flex>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
