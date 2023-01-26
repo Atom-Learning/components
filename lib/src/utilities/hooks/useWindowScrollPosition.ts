fb544c99d358ea9f4d0873447a0c788fa9736783
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-01-26 09:33:47 +0000

    refactor: revert to use of throttle in useWindowScrollPosition but allow for method override

--- lib/src/utilities/hooks/useWindowScrollPosition.ts
+++ lib/src/utilities/hooks/useWindowScrollPosition.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
-import { debounce } from 'throttle-debounce'
+import { debounce, throttle } from 'throttle-debounce'
 
-export const useWindowScrollPosition = ({ delay = 500 } = {}): {
+export const useWindowScrollPosition = ({ delay = 500, delayMethod = throttle }: { delay?: number, delayMethod?: throttle | debounce } = {}): {
   x: number
   y: number
 } => {
   const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 })
 
   useEffect(() => {
-    const handleScroll = debounce(delay, () => {
+    const handleScroll = delayMethod(delay, () => {
       setScrollPosition({ x: window.scrollX, y: window.scrollY })
     })
 
