372117aedea90d0a217bb54aed3e5922108d04e8
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-02-01 14:56:46 +0000

    refactor: useWindowScrollPosition change delayMethod type to string enum

--- lib/src/utilities/hooks/useWindowScrollPosition.ts
+++ lib/src/utilities/hooks/useWindowScrollPosition.ts
@@ -1,14 +1,19 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { debounce, throttle } from 'throttle-debounce'
 
-export const useWindowScrollPosition = ({ delay = 500, delayMethod = throttle }: { delay?: number, delayMethod?: throttle | debounce } = {}): {
+export const useWindowScrollPosition = ({ delay = 500, delayMethod = 'throttle' }: { delay?: number, delayMethod?: 'throttle' | 'debounce' } = {}): {
   x: number
   y: number
 } => {
   const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 })
 
+  const delayMethodFn = useMemo(() => {
+    if (delayMethod === 'throttle') return throttle
+    if (delayMethod === 'debounce') return debounce
+  }, [delayMethod])
+
   useEffect(() => {
-    const handleScroll = delayMethod(delay, () => {
+    const handleScroll = delayMethodFn(delay, () => {
       setScrollPosition({ x: window.scrollX, y: window.scrollY })
     })
 
@@ -17,7 +22,7 @@ export const useWindowScrollPosition = ({ delay = 500, delayMethod = throttle }:
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
-  }, [])
+  }, [delayMethodFn])
 
   return scrollPosition
 }
