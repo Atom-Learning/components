7b97fdcc5ec3f498ae540c6f1520cf9b38dfd5ce
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-02-02 16:38:04 +0000

    fix: pre markdown setup

--- documentation/components/markdown/index.tsx
+++ documentation/components/markdown/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 import { ComponentProps } from './editor-component/ComponentProps'
 import {
-    ColorTokenList,
+  ColorTokenList,
 } from './editor-component/ColorTokenList'
 import { InlineCode } from './InlineCode'
 import { CodeBlock } from './editor-component/CodeBlock'
@@ -16,63 +16,63 @@ import { RadiusTokenList } from './editor-component/RadiusTokenList'
 import { ShadowTokenList } from './editor-component/ShadowTokenList'
 
 export const components = {
-    h2: (props) => (
-        <Heading
-            {...props}
-            size="md"
-            as="h2"
-            css={{ fontWeight: 600 }}
-        />
-    ),
-    h3: (props) => (
-        <Heading {...props} as="h3" size="sm" />
-    ),
-    h4: (props) => (
-        <Heading {...props} as="h4" size="xs" />
-    ),
-    h5: (props) => (
-        <Heading {...props} as="h5" size="xs" />
-    ),
-    h6: (props) => (
-        <Heading {...props} as="h6" size="xs" />
-    ),
-    code: InlineCode,
-    pre: CodeBlock,
-    p: (props) => <Text {...props} />,
-    ul: (props) => <List {...props} />,
-    li: List.Item,
-    blockquote: (props) => (
-        <Text
-            {...props}
-            as="blockquote"
-            css={{
-                pl: '$5',
-                color: '$base7',
-                borderLeft: '2px solid $colors$base3'
-            }}
-        >
-            {props.children}
-        </Text>
-    ),
-    a: Link,
-    hr: (props) => <Divider {...props} />,
-    Text,
-    Box,
-    table: Table,
-    thead: Table.Header,
-    th: Table.HeaderCell,
-    tbody: Table.Body,
-    tr: Table.Row,
-    td: Table.Cell,
-    tfoot: Table.Footer,
-    ComponentProps,
-    CodeBlock,
-    ColorTokenList,
-    IconTokenList,
-    SizeTokenList,
-    SpaceTokenList,
-    FontFamilyTokenList,
-    FontSizeTokenList,
-    RadiusTokenList,
-    ShadowTokenList
+  h2: (props) => (
+    <Heading
+      {...props}
+      size="md"
+      as="h2"
+      css={{ fontWeight: 600 }}
+    />
+  ),
+  h3: (props) => (
+    <Heading {...props} as="h3" size="sm" />
+  ),
+  h4: (props) => (
+    <Heading {...props} as="h4" size="xs" />
+  ),
+  h5: (props) => (
+    <Heading {...props} as="h5" size="xs" />
+  ),
+  h6: (props) => (
+    <Heading {...props} as="h6" size="xs" />
+  ),
+  code: InlineCode,
+  pre: (props) => <CodeBlock code={props?.children?.props?.children} language={props?.children?.props?.className?.replace('language-', '')} />,
+  p: (props) => <Text {...props} />,
+  ul: (props) => <List {...props} />,
+  li: List.Item,
+  blockquote: (props) => (
+    <Text
+      {...props}
+      as="blockquote"
+      css={{
+        pl: '$5',
+        color: '$base7',
+        borderLeft: '2px solid $colors$base3'
+      }}
+    >
+      {props.children}
+    </Text>
+  ),
+  a: Link,
+  hr: (props) => <Divider {...props} />,
+  Text,
+  Box,
+  table: Table,
+  thead: Table.Header,
+  th: Table.HeaderCell,
+  tbody: Table.Body,
+  tr: Table.Row,
+  td: Table.Cell,
+  tfoot: Table.Footer,
+  ComponentProps,
+  CodeBlock,
+  ColorTokenList,
+  IconTokenList,
+  SizeTokenList,
+  SpaceTokenList,
+  FontFamilyTokenList,
+  FontSizeTokenList,
+  RadiusTokenList,
+  ShadowTokenList
 }
