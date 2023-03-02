e078c233feae6f0f17bf09ad9adda0811117139f
viniciusvolpe <viniciusvolpe@users.noreply.github.com>
2023-03-02 11:40:59 +0000

    chore: apply auto-fix changes

--- lib/src/components/table/TableStickyColumnsContainer.tsx
+++ lib/src/components/table/TableStickyColumnsContainer.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { CSS } from '~/stitches'
 
 import { Box } from '../box'
-
 import { useStickyColumnsCss } from './useStickyColumnsCss'
 interface ITableStickyColumnsContainerProps {
   children: React.ReactNode
