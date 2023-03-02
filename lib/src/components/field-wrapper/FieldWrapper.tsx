3587ab95f89afe48d9b9a726324ba13c839d6e2b
Mariandi Stylianou <mariandi.stylianou@atomlearning.co.uk>
2023-03-02 16:19:30 +0200

    fix(fieldwrapper): add opt chaining to prevent errors if value isn't passed

--- lib/src/components/field-wrapper/FieldWrapper.tsx
+++ lib/src/components/field-wrapper/FieldWrapper.tsx
@@ -53,7 +53,7 @@ export const FieldWrapper: React.FC<FieldWrapperProps> = ({
           {label}
         </Label>
         {prompt && (
-          <Link href={prompt.link} onClick={prompt.onClick} size="sm">
+          <Link href={prompt?.link} onClick={prompt?.onClick} size="sm">
             {prompt.label}
           </Link>
         )}
