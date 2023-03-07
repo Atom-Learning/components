f5bae519828fe45b860dcb15fd58139c5b9894d2
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-03-07 17:04:36 +0000

    fix: custom idColumn on DragAndDropTable

--- lib/src/components/data-table/drag-and-drop/DragAndDropTableBody.tsx
+++ lib/src/components/data-table/drag-and-drop/DragAndDropTableBody.tsx
@@ -7,17 +7,20 @@ import { DragAndDropTableRow } from './DragAndDropTableRow'
 type DataTableBodyProps = Omit<
   React.ComponentProps<typeof Table.Body>,
   'children'
->
+> & { idColumn?: string }
 
 export const DragAndDropTableBody: React.FC<DataTableBodyProps> = ({
   striped = false,
+  idColumn = 'id',
   ...props
 }) => {
   const { getRowModel } = useDataTable()
   return (
     <Table.Body {...props} striped={striped}>
       {getRowModel().rows.map((row) => {
-        return <DragAndDropTableRow row={row} key={row.id} />
+        return (
+          <DragAndDropTableRow row={row} key={row.id} idColumn={idColumn} />
+        )
       })}
     </Table.Body>
   )
