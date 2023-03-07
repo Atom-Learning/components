7b60b8be02901c9c373ad7a462de73a83798f7cb
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-03-07 13:58:10 +0000

    feat: split Sortable out of DataTable into a reusable primitive

--- lib/src/components/data-table/drag-and-drop/DragAndDropTable.tsx
+++ lib/src/components/data-table/drag-and-drop/DragAndDropTable.tsx
@@ -1,25 +1,31 @@
 import * as React from 'react'
 
 import { Table } from '../../table'
+import { Sortable } from '../../sortable'
 import { DataTable } from '../DataTable'
 import { AsyncDataState, TAsyncDataResult } from '../DataTable.types'
 import { useDataTable } from '../DataTableContext'
 import { DataTableLoading } from '../DataTableLoading'
+import { arrayMove } from '@dnd-kit/sortable'
 import type { DataTableTableProps } from '../DataTableTable'
-import { DragAndDropContainer } from './DragAndDropContainer'
 import { DragAndDropTableBody } from './DragAndDropTableBody'
 
 type DragAndDropTableProps = DataTableTableProps & {
   idColumn?: string
-  onDragAndDrop?: (
-    oldIndex: number,
-    newIndex: number,
-    newData: Record<string, unknown>[]
-  ) => void
+  onDragAndDrop?: (onDragAndDropData: {
+    oldIndex: number
+    newIndex: number
+    newData: TAsyncDataResult
+  }) => void
 }
 
+export const getRowOrder = (data: TAsyncDataResult, idColumn: string) =>
+  data.results.map((row) => {
+    return row[idColumn]
+  })
+
 export const DragAndDropTable: React.FC<DragAndDropTableProps> = ({
-  idColumn,
+  idColumn = 'id',
   onDragAndDrop,
   sortable,
   striped,
@@ -27,11 +33,28 @@ export const DragAndDropTable: React.FC<DragAndDropTableProps> = ({
   css,
   ...props
 }) => {
-  const { asyncDataState } = useDataTable()
+  const { asyncDataState, data, setData } = useDataTable()
   const isPending = asyncDataState === AsyncDataState.PENDING
 
+  const rowIds = React.useMemo(
+    () =>
+      data.results.map((row) => {
+        return row[idColumn] as React.ReactText
+      }),
+    [data]
+  )
+  const handleSortChange = React.useCallback(
+    ({ oldIndex, newIndex }) => {
+      const sortedResults = arrayMove(data.results, oldIndex, newIndex)
+      const newData = { results: sortedResults, total: data.total }
+      setData({ results: sortedResults, total: data.total })
+      onDragAndDrop?.({ oldIndex, newIndex, newData })
+    },
+    [data, onDragAndDrop]
+  )
+
   return (
-    <DragAndDropContainer onChange={onDragAndDrop} idColumn={idColumn}>
+    <Sortable.Root onSortChange={handleSortChange} sortableIds={rowIds}>
       <DataTableLoading />
       <Table
         {...props}
@@ -47,6 +70,6 @@ export const DragAndDropTable: React.FC<DragAndDropTableProps> = ({
         <DataTable.Head theme={theme} sortable={sortable} />
         <DragAndDropTableBody striped={striped} />
       </Table>
-    </DragAndDropContainer>
+    </Sortable.Root>
   )
 }
---
+++ lib/src/components/sortable/index.ts
@@ -0,0 +1,5 @@
+import { SortableRoot as Root } from './SortableRoot'
+import { SortableItem as Item } from './SortableItem'
+import { SortableHandle as Handle } from './SortableHandle'
+
+export const Sortable = { Root, Item, Handle }
