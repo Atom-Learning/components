f97df9d6fc4f45987dae88c95f45cbf65102ca0a
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-03-10 17:07:03 +0000

    feat: add Dos and Donts editor component

---
+++ documentation/components/markdown/editor-component/DosAndDonts.tsx
@@ -0,0 +1,52 @@
+import {
+  Image,
+  Text,
+  Grid,
+  Box,
+  Heading
+} from '@atom-learning/components'
+import * as React from 'react'
+
+type TDosAndDontsItemProps = { image: string, type: 'do' | 'avoid' | 'dont', description: string }
+
+const DosAndDontsItem: React.FC<TDosAndDontsItemProps> = ({ image, type, description }) => {
+  const typeBorderColor = type === 'do' ? '$success' : type === 'dont' ? '$danger' : '$warning'
+  const typeText = type === 'do' ? 'Do' : type === 'dont' ? "Don't" : 'Avoid'
+  return (
+    <Box as="li" css={{ listStyle: 'none' }}>
+      <Box css={{
+        aspectRatio: "332/192",
+        background: '$base1',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottom: '4px solid',
+        borderColor: typeBorderColor,
+        mb: '$2',
+        p: '$2'
+      }}>
+        <Image src={image} />
+      </Box>
+      <Heading as="h3" size="xs" css={{ mb: '$3' }}>{typeText}</Heading>
+      <Text>{description}</Text>
+    </Box >
+  )
+}
+
+type DosAndDontsProps = {
+  items: TDosAndDontsItemProps[]
+}
+
+export const DosAndDonts: React.FC<DosAndDontsProps> = ({ items }) => {
+
+  return (
+    <Grid
+      css={{
+        gap: '$5 $4',
+        gridTemplateColumns: '1fr',
+        '@sm': { gridTemplateColumns: '1fr 1fr' },
+      }}>
+      {items.map((props) => <DosAndDontsItem {...props} />)}
+    </Grid>
+  )
+}
