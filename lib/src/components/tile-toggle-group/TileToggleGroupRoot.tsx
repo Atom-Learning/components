d13a7f9ded696d44ec7b86dc528e3be04af74e8e
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-04-12 15:53:57 +0100

    fix: ts fix/hack

--- lib/src/components/tile-toggle-group/TileToggleGroupRoot.tsx
+++ lib/src/components/tile-toggle-group/TileToggleGroupRoot.tsx
@@ -1,26 +1,30 @@
 import * as ToggleGroup from '@radix-ui/react-toggle-group'
 import * as React from 'react'
+
 import { TileGroup } from '~/components/tile'
 
 type TTileToggleGroupRootProps = React.ComponentProps<typeof TileGroup> &
   React.ComponentProps<typeof ToggleGroup.Root>
 
 const orientationToDirection = (orientation) =>
-  orientation === 'horizontal' ? 'row' : 'column'
+  orientation === 'horizontal'
+    ? 'row'
+    : orientation === 'vertical'
+    ? 'column'
+    : undefined
 
-export const TileToggleGroupRoot: React.ForwardRefExoticComponent<TTileToggleGroupRootProps> =
-  React.forwardRef<HTMLDivElement, TTileToggleGroupRootProps>(
-    ({ orientation = 'horizontal', ...rest }, ref) => {
-      const direction = orientationToDirection(orientation)
-      return (
-        <TileGroup
-          ref={ref}
-          direction={direction}
-          orientation={orientation}
-          align={false}
-          as={ToggleGroup.Root}
-          {...rest}
-        />
-      )
-    }
+export const TileToggleGroupRoot = React.forwardRef<
+  HTMLDivElement,
+  TTileToggleGroupRootProps
+>((props, ref) => {
+  const direction = orientationToDirection(props.orientation)
+  return (
+    <TileGroup
+      ref={ref}
+      as={ToggleGroup.Root}
+      direction={direction}
+      align={false}
+      {...props}
+    />
   )
+})
