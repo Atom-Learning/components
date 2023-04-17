5e7dad7eba536d3aa927fa1cf3022820b54a3d40
Avinash Verma <avinash.verma@atomlearning.co.uk>
2023-04-17 11:41:55 +0100

    chore: hide image for sm variant

--- lib/src/components/banner/BannerImage.tsx
+++ lib/src/components/banner/BannerImage.tsx
@@ -3,10 +3,14 @@ import * as React from 'react'
 import { Box } from '../box'
 import { Flex } from '../flex'
 import { Image } from '../image'
+import { useBannerContext } from './BannerContext'
 
 export const BannerImage: React.FC<
   Pick<React.ComponentProps<typeof Image>, 'src'>
 > = ({ src }) => {
+  const { size } = useBannerContext()
+  if (size === 'sm') return null
+
   return (
     <Flex
       css={{
