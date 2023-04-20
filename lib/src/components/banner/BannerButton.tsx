9d210923216b6582d4402331b890b5611c9f31a4
Avinash Verma <avinash.verma@atomlearning.co.uk>
2023-04-20 05:38:32 +0100

    chore: remove redundant code

--- lib/src/components/banner/BannerButton.tsx
+++ lib/src/components/banner/BannerButton.tsx
@@ -3,20 +3,12 @@ import * as React from 'react'
 import { Button } from '../button'
 import { useBannerContext } from './BannerContext'
 
-export const BannerButton: React.FC<React.ComponentProps<typeof Button>> = ({
-  appearance,
-  ...props
-}) => {
+export const BannerButton: React.FC<React.ComponentProps<typeof Button>> = (
+  props
+) => {
   const { emphasis, size } = useBannerContext()
 
-  return appearance === 'outline' ? (
-    <Button
-      size={size}
-      appearance={appearance}
-      theme={emphasis === 'hiContrast' ? 'neutral' : 'primary'}
-      {...props}
-    />
-  ) : (
+  return (
     <Button
       size={size}
       theme={emphasis === 'hiContrast' ? 'neutral' : 'primary'}
