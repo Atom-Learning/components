31ad7ed0628b2bf96e3f1b321007d43f8c945746
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-04-21 15:01:20 +0100

    feat: store selected tab in the url in documentation site

--- documentation/components/page/Layout.tsx
+++ documentation/components/page/Layout.tsx
@@ -5,14 +5,28 @@ import { Container, Header, Footer, Links, Pagination } from '.'
 import { components } from '~/components/markdown/index'
 import { MDXRemote } from 'next-mdx-remote'
 import { TDynamicPage } from '~/lib/types/DynamicPage'
+import { useQueryParams } from '~/utilities/hooks/useQueryParams'
+import { useEffect, useState } from 'react'
+import kebabCase from 'lodash.kebabcase'
 
 export const Layout: React.FC<TDynamicPage> = (props) => {
   const { links, slug, tabs, title } = props
   const tabsLength = tabs?.length
   const hasContent = !!tabsLength
+  const { queryParams, updateQueryParams } = useQueryParams()
+
+  const [tab, setTab] = useState(kebabCase(tabs?.[0]?.title))
+  useEffect(() => {
+    if (queryParams.tab) setTab(queryParams.tab)
+  }, [queryParams.tab])
+
+  const isShowingTabs = tabs.length > 1
+  useEffect(() => {
+    if (isShowingTabs) updateQueryParams({ tab }, { method: 'replace', debounce: 0 })
+  }, [isShowingTabs, tab, updateQueryParams])
 
   return (
-    <Tabs defaultValue="tab0">
+    <Tabs value={tab} onValueChange={setTab}>
       <Head>
         <title>{title ? `${title} | ` : ''}Atom Learning Design System</title>
       </Head>
@@ -26,10 +40,10 @@ export const Layout: React.FC<TDynamicPage> = (props) => {
               {title}
             </Heading>
             <Links {...links} />
-            {tabsLength > 1 && (
+            {isShowingTabs && (
               <Tabs.TriggerList css={{ position: 'absolute', bottom: 0 }}>
-                {tabs.map((tab, i) => (
-                  <Tabs.Trigger key={tab.title} value={`tab${i}`}>
+                {tabs.map((tab) => (
+                  <Tabs.Trigger key={tab.title} value={kebabCase(tab.title)}>
                     {tab.title}
                   </Tabs.Trigger>
                 ))}
@@ -40,8 +54,8 @@ export const Layout: React.FC<TDynamicPage> = (props) => {
 
         {hasContent && (
           <Container css={{ py: '$5' }}>
-            {tabs.map((tab, i) => (
-              <Tabs.Content key={tab.title} value={`tab${i}`}>
+            {tabs.map((tab) => (
+              <Tabs.Content key={tab.title} value={kebabCase(tab.title)}>
                 <StackContent>
                   <MDXRemote {...tab.content} components={components} />
                 </StackContent>
