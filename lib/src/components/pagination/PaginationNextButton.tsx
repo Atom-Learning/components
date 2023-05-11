f9803965c5f446ec57bdb7b4f6bffc273ee9016e
Mzingaye Nkala <mzingaye.nkala@atomlearning.co.uk>
2023-05-11 10:07:19 +0000

    feat: add navigation buttons for pagination

---
+++ lib/src/components/pagination/PaginationNextButton.tsx
@@ -0,0 +1,34 @@
+import { ChevronRight } from '@atom-learning/icons'
+import * as React from 'react'
+
+import { CSS, styled } from '../../stitches'
+import { ActionIcon } from '../action-icon'
+import { Icon } from '../icon'
+import { usePagination } from './pagination-context/PaginationContext'
+
+export const PaginationNextButton: React.FC<{
+  onClick?: (callback: () => void) => void
+  css: CSS
+}> = ({ onClick, css }) => {
+  const { goToNextPage, currentPage, numOfPages } = usePagination()
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick(goToNextPage)
+    }
+    goToNextPage?.()
+  }
+
+  return (
+    <ActionIcon
+      hasTooltip={false}
+      size="md"
+      theme="neutral"
+      onClick={handleClick}
+      disabled={currentPage === numOfPages}
+      css={css}
+    >
+      <Icon is={ChevronRight} />
+    </ActionIcon>
+  )
+}
---
+++ lib/src/components/pagination/PaginationPreviousButton.tsx
@@ -0,0 +1,34 @@
+import { ChevronLeft } from '@atom-learning/icons'
+import * as React from 'react'
+
+import { CSS, styled } from '../../stitches'
+import { ActionIcon } from '../action-icon'
+import { Icon } from '../icon'
+import { usePagination } from './pagination-context/PaginationContext'
+
+export const PaginationPreviousButton: React.FC<{
+  onClick?: (callback: () => void) => void
+  css: CSS
+}> = ({ onClick, css }) => {
+  const { goToPreviousPage, currentPage } = usePagination()
+
+  const handleClick = () => {
+    if (onClick) {
+      onClick(goToPreviousPage)
+    }
+    goToPreviousPage?.()
+  }
+
+  return (
+    <ActionIcon
+      hasTooltip={false}
+      size="md"
+      theme="neutral"
+      onClick={handleClick}
+      css={css}
+      disabled={currentPage === 1}
+    >
+      <Icon is={ChevronLeft} />
+    </ActionIcon>
+  )
+}
