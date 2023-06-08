0731a8bd5949758f2aa89ff5c58b629ddf63cbd8
Thomas Digby <tom@atomlearning.co.uk>
2023-06-08 16:19:53 +0100

    feat: replace custom margin gap with native flex gap

--- lib/src/components/stack/Stack.test.tsx
+++ lib/src/components/stack/Stack.test.tsx
@@ -2,10 +2,17 @@ import { render } from '@testing-library/react'
 import * as React from 'react'
 
 import { Stack } from '.'
+import { Box } from '../box'
 
 describe('Stack component', () => {
   it('renders', async () => {
-    const { container } = render(<Stack />)
+    const { container } = render(
+      <Stack>
+        <Box css={{ size: 100, bg: 'red' }} />
+        <Box css={{ size: 100, bg: 'red' }} />
+        <Box css={{ size: 100, bg: 'red' }} />
+      </Stack>
+    )
 
     expect(container).toMatchSnapshot()
   })
--- lib/src/components/stack/Stack.tsx
+++ lib/src/components/stack/Stack.tsx
@@ -1,100 +1,65 @@
 import * as React from 'react'
 
-import { CSS, styled } from '~/stitches'
+import { styled } from '~/stitches'
 import { createThemeVariants } from '~/utilities'
-import { CSSWrapper } from '~/utilities/css-wrapper'
 
-const StyledStack = styled('div', {
-  display: 'flex',
-  '& > *': {
-    m: 0
-  },
+import { Flex } from '../flex'
+
+const StyledStack = styled(Flex, {
   variants: {
     direction: {
-      row: {
-        flexDirection: 'row'
-      },
-      'row-reverse': {
-        flexDirection: 'row-reverse'
-      },
-      column: {
-        flexDirection: 'column'
-      }
+      row: { flexDirection: 'row' },
+      'row-reverse': { flexDirection: 'row-reverse' },
+      column: { flexDirection: 'column' }
     },
     wrap: {
-      wrap: {
-        flexWrap: 'wrap'
-      },
-      'no-wrap': {
-        flexWrap: 'no-wrap'
-      },
-      'wrap-reverse': {
-        flexWrap: 'wrap-reverse'
-      }
+      wrap: { flexWrap: 'wrap' },
+      'no-wrap': { flexWrap: 'no-wrap' },
+      'wrap-reverse': { flexWrap: 'wrap-reverse' }
     },
     justify: {
       start: { justifyContent: 'flex-start' },
       center: { justifyContent: 'center' },
-      end: { justifyContent: 'flex-end' },
-      false: {}
+      end: { justifyContent: 'flex-end' }
     },
     align: {
       start: { alignItems: 'flex-start' },
       center: { alignItems: 'center' },
-      end: { alignItems: 'flex-end' },
-      false: {}
+      end: { alignItems: 'flex-end' }
     },
-    gap: {
-      ...createThemeVariants('space', {
-        mt: '-$key',
-        ml: '-$key',
-        '& > *': {
-          mt: '$key',
-          ml: '$key'
-        }
-      }),
-      false: {}
-    }
+    gap: createThemeVariants('space', { gap: '$key' })
   }
 })
 
-type StackPropsType = React.ComponentProps<typeof StyledStack> & {
-  css?: CSS
-  as?: any
-} // (!) `css` and `as` are both props that come from `stitches`. It would be better to figure out and export the appropriate type for them in stitches!
-
-export const Stack: React.ForwardRefExoticComponent<StackPropsType> =
-  React.forwardRef(
-    (
-      {
-        css,
-        gap = 2,
-        direction = 'row',
-        wrap = 'wrap',
-        justify = 'start',
-        align,
-        ...remainingProps
-      },
-      ref
-    ) => {
-      return (
-        <CSSWrapper css={css}>
-          <StyledStack
-            ref={ref}
-            direction={direction}
-            gap={gap}
-            wrap={wrap}
-            justify={justify}
-            align={
-              typeof align === 'undefined' && direction !== 'column'
-                ? 'center'
-                : align
-            }
-            {...remainingProps}
-          />
-        </CSSWrapper>
-      )
-    }
+export const Stack = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<typeof StyledStack>
+>(
+  (
+    {
+      gap = 2,
+      direction = 'row',
+      wrap = 'wrap',
+      justify = 'start',
+      align,
+      ...remainingProps
+    },
+    ref
+  ) => (
+    <StyledStack
+      ref={ref}
+      direction={direction}
+      gap={gap}
+      wrap={wrap}
+      justify={justify}
+      align={
+        typeof align === 'undefined' && direction !== 'column'
+          ? 'center'
+          : align
+      }
+      {...remainingProps}
+    />
   )
+)
 
 Stack.displayName = 'Stack'
