bdad9b33e040f4712c614bc76a5811e739f51d66
Marcdup1 <marc.duplessis1@gmail.com>
2023-07-05 14:03:25 +0100

    feat: add create password field component

---
+++ documentation/content/components.form.fields.create-password-field.md
@@ -0,0 +1,49 @@
+---
+slug: create-password-field
+title: Create Password Field
+links:
+  viewSource: components/create-password-field
+  showReportAnIssue: true
+tabs:
+  - content: >-
+      `CreatePasswordField` is the preferred way to render a form field for
+      creating a new password. It is built on top of the `PasswordField`.
+
+
+      `CreatePasswordField` accepts the same props as `PasswordInput` but provides default values for some of them (e.g. `label` defaults to `"Create a password"` and `name` defaults to `password`).
+
+
+      `CreatePasswordField` introduces a `validate` prop. `validate` can be synchronous or asynchronous, it allows you to provide a custom function to handle password validation according to your specific requirements. The `defaultValidation` prop allows you to provide an initial validation status that matches the same structure that the validate function returns. This way, you can display immediate feedback to the user while the actual validation is being processed asynchronously.
+
+
+      To provide visual feedback on the validation rules, `CreatePasswordField` utilizes the `InlineMessage` component. The direction of these messages can be controlled using the `messageDirection` prop.
+
+      <CodeBlock live={true} preview={true} code={`<Form onSubmit={console.log}>
+        <CreatePasswordField
+          validate={(password) => ({
+            ['7+ characters']: password.length >= 7,
+            ['Is not common password']: !['1234', 'password'].includes(password)
+          })}
+          defaultValidation={{
+            ['7+ characters']: false,
+            ['Is not common password']: false,
+          }}
+          messageDirection="column"
+          css={{ width: 320 }}
+        />
+      </Form>`} language={"tsx"} />
+
+
+      ## API Reference
+
+
+      <ComponentProps component="CreatePasswordField" />
+    title: Main
+parent: ru0Ovr_U82kdQX8m3WahL
+uuid: MD1mmrf40ecDo4z16-bco
+nestedSlug:
+  - components
+  - form
+  - fields
+  - create-password-field
+---
---
+++ lib/src/components/create-password-field/CreatePasswordField.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react'
+import { useFormContext } from 'react-hook-form'
+import { throttle } from 'throttle-debounce'
+import invariant from 'invariant'
+
+import type { CSS } from '~/stitches'
+
+import { FieldElementWrapperProps } from '../field-wrapper'
+import { Flex } from '../flex'
+import { InlineMessage } from '../inline-message'
+import { PasswordField } from '../password-field'
+import { PasswordInput } from '../password-input'
+
+type ValidationResult = Record<string, boolean>
+
+type CreatePasswordFieldProps = React.ComponentProps<typeof PasswordInput> &
+  Omit<FieldElementWrapperProps, 'label' | 'name'> & {
+    label?: string
+    name?: string
+    validate: (
+      password: string
+    ) => Promise<ValidationResult | undefined> | ValidationResult
+    defaultValidation: ValidationResult
+    messageDirection?: CSS['flexDirection']
+  }
+
+export const CreatePasswordField = ({
+  validate,
+  defaultValidation,
+  messageDirection = 'row',
+  label = 'Create a password',
+  name = 'password',
+  css,
+  validation,
+  ...remainingProps
+}: CreatePasswordFieldProps) => {
+  const { formState } = useFormContext()
+  const [isFocused, setIsFocused] = React.useState<boolean>(false)
+  const [validationResult, setValidationResult] =
+    React.useState<ValidationResult>(defaultValidation)
+
+  const touched = formState.touched[name]
+  const error = formState.errors[name]?.type === 'validate'
+
+  const validatePassword = React.useCallback(
+    async (password: string) => {
+      const result = await validate(password)
+
+      if (result) {
+        invariant(
+          typeof result === 'object',
+          'The validate function must return an object'
+        )
+
+        setValidationResult(result)
+        return Object.entries(result).every(([_, isValid]) => isValid)
+      }
+
+      return false
+    },
+    [setValidationResult]
+  )
+
+  const handleChange = React.useCallback(throttle(500, validatePassword), [
+    validatePassword
+  ])
+
+  return (
+    <>
+      <PasswordField
+        label={label}
+        name={name}
+        css={css}
+        onChange={(e) => handleChange(e.target.value)}
+        onBlur={() => setIsFocused(false)}
+        onFocus={() => setIsFocused(true)}
+        validation={{ ...validation, validate: validatePassword }}
+        {...remainingProps}
+      />
+      {(touched || isFocused || error) && (
+        <Flex
+          css={{
+            mt: '$2',
+            mb: '$4',
+            gap: '$2',
+            flexDirection: messageDirection,
+          }}
+        >
+          {Object.entries(validationResult).map(([message, result]) => (
+            <InlineMessage
+              key={message}
+              theme={result ? 'success' : isFocused ? 'neutral' : 'error'}
+            >
+              {message}
+            </InlineMessage>
+          ))}
+        </Flex>
+      )}
+    </>
+  )
+}
+
+CreatePasswordField.displayName = 'CreatePasswordField'
