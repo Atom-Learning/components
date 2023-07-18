5a8e0519d909573cc1d2d7827838b8f46f524aa9
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-07-18 16:05:01 +0100

    feat: Flex but kind of like the old Stack

--- lib/src/components/flex/Flex.tsx
+++ lib/src/components/flex/Flex.tsx
@@ -1,5 +1,42 @@
-import { styled } from '~/stitches'
+import * as React from 'react'
 
-export const Flex = styled('div', { display: 'flex' })
+import { CSS, styled } from '~/stitches'
+import { createThemeVariants } from '~/utilities'
+
+const createVariants = <T extends GlobalValue[]>(variants: T, fn: (string: T[number]) => CSS) => {
+  return variants.reduce((prev, variant) => ({ ...prev, [variant]: fn(variant) }), {} as Record<T[number], CSS>)
+}
+
+const globalValues = ['inherit', 'initial', 'revert', 'revert-layer', 'unset'] as const
+
+/*
+ * The following type is partially a hack to get the rest of the createVariants array parameter to be recognised as const.
+ * Thus giving is the correct types generated for these generated variant props.
+ * No clue how we can do it cleaner.
+ * Mad props to: Elliot for getting this to working as is.
+ *
+*/
+// eslint-disable-next-line @typescript-eslint/ban-types
+type GlobalValue = typeof globalValues[number] | string & {}
+
+
+const StyledFlex = styled('div', {
+  unset: 'all',
+  display: 'flex',
+  variants: {
+    direction: createVariants([...globalValues, 'row', 'row-reverse', 'column', 'column-reverse'], (v) => { return { flexDirection: v } }),
+    wrap: createVariants([...globalValues, 'nowrap', 'wrap', 'wrap-reverse'], (v) => { return { flexWrap: v } }),
+    // Why is both `start` and `flex-start` valid values? Answer: https://csslayout.news/whats-the-difference-between-the-alignment-values-of-start-flex-start-and-self-start/
+    justify: createVariants([...globalValues, 'normal', 'unsafe', 'safe', 'start', 'center', 'end', 'flex-start', 'flex-end', 'left', 'right', 'space-between', 'space-around', 'space-evenly', 'stretch'], (v) => { return { justifyContent: v } }),
+    align: createVariants([...globalValues, 'normal', 'unsafe', 'safe', 'center', 'start', 'end', 'self-start', 'self-end', 'flex-start', 'flex-end', 'baseline', 'first baseline', 'last baseline', 'stretch'], (v) => { return { alignItems: v } }),
+    gap: createThemeVariants('space', { gap: '$key' })
+  }
+})
+
+type TFlexProps = React.ComponentProps<typeof StyledFlex>
+
+export const Flex = React.forwardRef<HTMLDivElement, TFlexProps>(
+  (props, ref) => <StyledFlex ref={ref} {...props} />
+)
 
 Flex.displayName = 'Flex'
