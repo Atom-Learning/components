eb4a0492df7e414f311de7f4d6c553837ad14453
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-07-25 20:03:26 +0100

    feat: update Flex to do all that Stack does

--- lib/src/components/flex/Flex.test.tsx
+++ lib/src/components/flex/Flex.test.tsx
@@ -4,17 +4,19 @@ import * as React from 'react'
 
 import { Flex } from '.'
 
+const FlexImplementation = (props) => {
+  return <Flex align="center" justify="center" {...props} />
+}
+
 describe(`Flex component`, () => {
   it('renders', async () => {
-    const { container } = render(
-      <Flex css={{ m: 'auto', height: 100, width: 100 }}>FLEX</Flex>
-    )
-    await screen.findByText('FLEX')
+    const { container } = render(<FlexImplementation>Flex</FlexImplementation>)
+    await screen.findByText('Flex')
     expect(container).toMatchSnapshot()
   })
 
   it('has no programmatically detectable a11y issues', async () => {
-    render(<Flex />)
+    render(<FlexImplementation />)
 
     const results = await axe(document.body)
     expect(results).toHaveNoViolations()
