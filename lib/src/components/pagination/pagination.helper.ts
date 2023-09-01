8707f69535120f975b611598c491ff0358cb37b0
Thomas Digby <tom@atomlearning.co.uk>
2023-09-01 10:17:32 +0100

    chore: add comments

--- lib/src/components/pagination/pagination.helper.ts
+++ lib/src/components/pagination/pagination.helper.ts
@@ -40,9 +40,19 @@ export const getPaginationItemsToRender = (
    * We need to remove `2` from `visibleElementsCount` to account
    * for the previous & next buttons
    *
-   *  +---+  +---+  +---+  +---+
-   *  | 1 |  | 2 |  | 3 |  | 4 |
-   *  +---+  +---+  +---+  +---+
+   *  pagesCount: 4
+   *  visibleElementsCount: 6 // VisibleElementsAmount.LESS
+   *  returns [1, 2, 3, 4]
+   *  +---+  +---+  +---+  +---+  +---+  +---+
+   *  | < |  | 1 |  | 2 |  | 3 |  | 4 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+
+   *
+   *  pagesCount: 6
+   *  visibleElementsCount:8 // VisibleElementsAmount.MORE
+   *  returns [1, 2, 3, 4, 5, 6]
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +---+  +---+
+   *  | < |  | 1 |  | 2 |  | 3 |  | 4 |  | 5 |  | 6 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +---+  +---+
    */
   if (pagesCount <= visibleElementsCount - 2) {
     return paginationItems
@@ -55,16 +65,16 @@ export const getPaginationItemsToRender = (
    *  currentPage: 1/2
    *  visibleElementsCount: 6 // VisibleElementsAmount.LESS
    *  returns [1, 2]
-   *  +---+  +---+  +---+  +---+
-   *  | 1 |  | 2 |  | … |  | 6 |
-   *  +---+  +---+  +---+  +---+
+   *  +---+  +---+  +---+  +---+  +---+  +---+
+   *  | < |  | 1 |  | 2 |  | … |  | 6 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+
    *
    *  currentPage: 1/2/3
    *  visibleElementsCount: 8 // VisibleElementsAmount.MORE
    *  returns [1, 2, 3, 4]
-   *  +---+  +---+  +---+  +---+  +---+  +----+
-   *  | 1 |  | 2 |  | 3 |  | 4 |  | … |  | 10 |
-   *  +---+  +---+  +---+  +---+  +---+  +----+
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +----+  +---+
+   *  | < |  | 1 |  | 2 |  | 3 |  | 4 |  | … |  | 10 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +----+  +---+
    */
   if (
     visibleElementsCount === VisibleElementsAmount.MORE
@@ -82,16 +92,16 @@ export const getPaginationItemsToRender = (
    *  currentPage: 7
    *  visibleElementsCount: 6 // VisibleElementsAmount.LESS
    *  returns [7, 8]
-   *  +---+  +---+  +---+  +---+
-   *  | 1 |  | … |  | 7 |  | 8 |
-   *  +---+  +---+  +---+  +---+
+   *  +---+  +---+  +---+  +---+  +---+  +---+
+   *  | < |  | 1 |  | … |  | 7 |  | 8 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+
    *
    *  currentPage: 7
    *  visibleElementsCount: 8 // VisibleElementsAmount.MORE
    *  returns [7, 8, 9, 10]
-   *  +---+  +---+  +---+  +---+  +---+  +---+
-   *  | 1 |  | … |  | 7 |  | 8 |  | 9 |  | 10 |
-   *  +---+  +---+  +---+  +---+  +---+  +---+
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +----+  +---+
+   *  | < |  | 1 |  | … |  | 7 |  | 8 |  | 9 |  | 10 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +----+  +---+
    */
   if (currentPage > pagesCount - paginationItemsLimit) {
     return paginationItems.slice(-paginationItemsLimit)
@@ -105,16 +115,16 @@ export const getPaginationItemsToRender = (
    *  currentPage: 4
    *  visibleElementsCount: 6 // VisibleElementsAmount.LESS
    *  returns [3, 4]
-   *  +---+  +---+  +---+  +---+
-   *  | 3 |  | 4 |  | … |  | 6 |
-   *  +---+  +---+  +---+  +---+
+   *  +---+  +---+  +---+  +---+  +---+  +---+
+   *  | < |  | 3 |  | 4 |  | … |  | 6 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+
    *
    *  currentPage: 6
    *  visibleElementsCount: 8 // VisibleElementsAmount.MORE
    *  returns [4, 5, 6, 7]
-   *  +---+  +---+  +---+  +---+  +---+  +---+
-   *  | 4 |  | 5 |  | 6 |  | 7 |  | … |  | 10 |
-   *  +---+  +---+  +---+  +---+  +---+  +---+
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +----+  +---+
+   *  | < |  | 4 |  | 5 |  | 6 |  | 7 |  | … |  | 10 |  | > |
+   *  +---+  +---+  +---+  +---+  +---+  +---+  +----+  +---+
    */
   return paginationItems.slice(
     visibleElementsCount === VisibleElementsAmount.MORE
