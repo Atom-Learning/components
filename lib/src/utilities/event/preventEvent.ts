2be7ece49ad572a91b2e94a85547c69d5f6ddaa3
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2023-09-22 14:15:30 +0100

    refactor: move preventHover to utils and rename to preventEvent to reuse in other components

--- lib/src/utilities/event/preventEvent.ts
+++ lib/src/utilities/event/preventEvent.ts
@@ -2,7 +2,7 @@
 Used to block default radix hover to open menu behaviour
 Props: https://github.com/radix-ui/primitives/issues/1630
 */
-export const preventHover = (event: any) => {
+export const preventEvent = (event: unknown): void => {
   const e = event as Event
   e.preventDefault()
 }
