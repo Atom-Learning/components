89975f10a65a8b4ac235222a450f77a69772b871
Thomas Digby <tom@atomlearning.co.uk>
2024-01-10 10:14:06 +0000

    fix: button apperance overlap

--- documentation/components/markdown/editor-component/Cards.tsx
+++ documentation/components/markdown/editor-component/Cards.tsx
@@ -39,7 +39,6 @@ const CardsItem: React.FC<TCardsItemProps> = ({
     : {}
 
   return (
-    // @ts-expect-error: is missing the following properties from type (252).
     <TileInteractive
       border
       borderRadius="md"
@@ -172,7 +171,6 @@ export const Cards: React.FC<CardsProps> = ({
             justify="center"
             type="multiple"
             value={selectedTags}
-            gap={2}
             onValueChange={handleSelectedTagsValueChange}
           >
             <ChipToggleGroup.Item value="all">All</ChipToggleGroup.Item>
