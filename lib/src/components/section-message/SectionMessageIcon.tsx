3a942319adf3115b3aa8b49e099de538c5f3fd46
Michal Rozenek <rozenekmichal@gmail.com>
2024-02-12 15:20:05 +0100

    fix: section message icon spacing

--- lib/src/components/section-message/SectionMessageIcon.tsx
+++ lib/src/components/section-message/SectionMessageIcon.tsx
@@ -32,7 +32,7 @@ export const SectionMessageIcon = ({
         m: 'auto',
         position: 'absolute',
         left: '$4',
-        top: '$4',
+        top: 'calc($4 - 2px)',
         color: 'currentColor',
         ...css
       }}
