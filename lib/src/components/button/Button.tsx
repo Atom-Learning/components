b74cd60ab172188f21ab7c94ff5465d3da4e8a62
Alex Nemes <alex.nemes@atomlearning.co.uk>
2024-03-05 13:11:34 +0000

    feat: add xl sizes B2B-679

--- lib/src/components/button/Button.tsx
+++ lib/src/components/button/Button.tsx
@@ -104,6 +104,14 @@ export const StyledButton = styled('button', {
         px: '$5',
         gap: '$3',
         [`& ${StyledIcon}`]: { size: 22 }
+      },
+      xl: {
+        fontSize: '$lg',
+        lineHeight: 1.5,
+        height: '$6',
+        px: '$5',
+        gap: '$3',
+        [`& ${StyledIcon}`]: { size: 22 }
       }
     },
     isLoading: {
@@ -127,7 +135,7 @@ export const StyledButton = styled('button', {
     getButtonSolidVariant('warning', '$warning', '$warningMid', '$warningDark', '$tonal500'),
     getButtonSolidVariant('danger', '$danger', '$dangerMid', '$dangerDark'),
     getButtonSolidVariant('neutral', 'white', opacify('white', -0.1), opacify('white', -0.25), '$primary800'),
-    
+
     getButtonOutlineVariant('primary', '$primary800', '$primary900', '$primary1000'),
     getButtonOutlineVariant('secondary', '$primary1000', '$primary1100', '$primary1200'),
     getButtonOutlineVariant('neutral', 'white', opacify('white', -0.2), opacify('white', -0.35))
@@ -149,7 +157,8 @@ const LoaderContentsWrapper = styled('span', {
     size: {
       sm: { gap: '$2' },
       md: { gap: '$3' },
-      lg: { gap: '$3' }
+      lg: { gap: '$3' },
+      xl: { gap: '$3' }
     }
   },
   defaultVariants: {
