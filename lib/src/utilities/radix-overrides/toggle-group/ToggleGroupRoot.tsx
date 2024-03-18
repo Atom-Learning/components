505586e4cfbf8efb5fc0a8b78c592f69d3b58404
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2024-03-18 11:31:06 +0000

    chore: better types and silence radix typeerror in ToggleGroupRoot override

--- lib/src/utilities/radix-overrides/toggle-group/ToggleGroupRoot.tsx
+++ lib/src/utilities/radix-overrides/toggle-group/ToggleGroupRoot.tsx
@@ -5,24 +5,35 @@ type RootType = {
   disableDeselect?: boolean
 }
 
-export const ToggleGroupRoot: React.ForwardRefExoticComponent<
-  React.ComponentProps<typeof ToggleGroup.Root> & RootType
-> = React.forwardRef(
-  ({ disableDeselect = false, onValueChange, defaultValue, ...rest }, ref) => {
-    const [internalValue, setInternalValue] = React.useState(defaultValue)
-    const handleValueChange = (newValue) => {
-      if (disableDeselect && (newValue === '' || newValue.length === 0)) return
-      setInternalValue(newValue)
-      onValueChange?.(newValue)
-    }
+type ToggleGroupRootProps = React.ComponentProps<typeof ToggleGroup.Root> &
+  RootType
 
-    return (
-      <ToggleGroup.Root
-        ref={ref}
-        onValueChange={handleValueChange}
-        value={internalValue}
-        {...rest}
-      />
-    )
-  }
-)
+export const ToggleGroupRoot: React.ForwardRefExoticComponent<ToggleGroupRootProps> =
+  React.forwardRef(
+    (
+      { disableDeselect = false, onValueChange, defaultValue, ...rest },
+      ref
+    ) => {
+      const [internalValue, setInternalValue] =
+        React.useState<ToggleGroupRootProps['value']>(defaultValue)
+      const handleValueChange: ToggleGroupRootProps['onValueChange'] = (
+        newValue
+      ) => {
+        if (disableDeselect && (newValue === '' || newValue?.length === 0))
+          return
+        setInternalValue(newValue)
+        onValueChange?.(newValue)
+      }
+
+      return (
+        // eslint-disable-next-line
+        // @ts-ignore Radix types complain on properties depending on whether `type="single"` or `"multiple"`. Works correctly so muting.
+        <ToggleGroup.Root
+          ref={ref}
+          onValueChange={handleValueChange}
+          value={internalValue}
+          {...rest}
+        />
+      )
+    }
+  )
