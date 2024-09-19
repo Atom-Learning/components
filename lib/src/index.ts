a89e584f83cca63b3234f9b5f2d4107fde801f29
Myrto Kontouli <myrto.kontouli@atomlearning.co.uk>
2024-09-19 12:23:43 +0100

    feat: export radix overrides

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -5,6 +5,8 @@ export * from './stitches'
 export { ColorScheme } from './experiments/color-scheme'
 export type { TcolorScheme } from './experiments/color-scheme'
 
+export * as RadixOverrides from './utilities/radix-overrides'
+
 export {
   createThemeVariants,
   focusVisibleStyleBlock,
