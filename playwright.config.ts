5d9f8266b9d1b85a313fd8687bd37f3e9fe49498
Avinash Verma <avinash.verma@atomlearning.co.uk>
2024-12-17 09:36:43 +0000

    chore: disable trace files

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,11 +35,7 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on'
+    trace: 'off'
   },
 
   timeout: 5 * 60 * 60 * 1000,
